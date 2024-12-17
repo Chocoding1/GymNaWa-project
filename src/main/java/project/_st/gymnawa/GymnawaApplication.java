69044f99bddaef8120ab97440f19082c5b97fe9f
chocoding <galmeagi2@naver.com>
2024-12-17 15:24:49 +0900

    초기 세팅

---
+++ src/main/java/project/_st/gymnawa/GymnawaApplication.java
@@ -0,0 +1,13 @@
+package project._st.gymnawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymnawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/_st/gymnawa/GymnawaApplicationTests.java
@@ -0,0 +1,13 @@
+package project._st.gymnawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymnawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
