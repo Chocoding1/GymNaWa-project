b59e83f2b9cd3d85fb07e18624059b22171d8728
chocoding <galmeagi2@naver.com>
2024-12-18 10:20:46 +0900

    회원 기능 개발 완료

---
+++ src/test/java/project/gymnawa/service/MemberServiceTest.java
@@ -0,0 +1,40 @@
+package project.gymnawa.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    /**
+     * 회원가입 Test
+     * 중복 아이디 검증
+     */
+    @Test
+    void join() {
+        //given
+        new Member
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void findOne() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
+        //given
+
+        //when
+
+        //then
+    }
+}
