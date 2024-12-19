f9c24b3da85e6a017969bfcd7c1ae1dee275f2aa
chocoding <galmeagi2@naver.com>
2024-12-19 10:49:42 +0900

    편의 메서드 및 생성자 추가

--- src/main/java/project/gymnawa/domain/Trainer.java
+++ src/main/java/project/gymnawa/domain/Trainer.java
@@ -24,4 +24,21 @@ public class Trainer {
 
     @OneToMany(mappedBy = "trainer")
     private List<Review> reviews = new ArrayList<>();
+
+    // JPA는 기본 생성자 필수 (객체 동적 생성할 수도 있기 때문)
+    public Trainer() {
+    }
+
+    // 테스트용 생성자
+    public Trainer(String loginId, String password, String name) {
+        this.loginId = loginId;
+        this.password = password;
+        this.name = name;
+    }
+
+    // 편의 메서드
+    public void setGym(Gym gym) {
+        this.gym = gym;
+        gym.getTrainers().add(this);
+    }
 }
