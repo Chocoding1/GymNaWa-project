bc75913e8876840c6cb87714bb3267535d7b0654
chocoding <galmeagi2@naver.com>
2024-12-19 10:51:08 +0900

    헬스장별 트레이너 검색 기능 추가

--- src/test/java/project/gymnawa/service/TrainerServiceTest.java
+++ src/test/java/project/gymnawa/service/TrainerServiceTest.java
@@ -1,4 +1,28 @@
+package project.gymnawa.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
 class TrainerServiceTest {
-  
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findTrainers() {
+    }
+
+    @Test
+    void findTrainersByGym() {
+    }
 }
