10edc386f153f154c1ef02cf9417eb721c03e55e
chocoding <galmeagi2@naver.com>
2024-12-30 10:44:19 +0900

    로그인 서비스 추가

--- src/main/java/project/gymnawa/service/MemberService.java
+++ src/main/java/project/gymnawa/service/MemberService.java
@@ -1,13 +1,13 @@
 package project.gymnawa.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.gymnawa.domain.Member;
 import project.gymnawa.repository.MemberRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,6 +16,9 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    /**
+     * 회원가입 함수
+     */
     @Transactional
     public Long join(Member member) {
         // 중복 회원 검증 필요
@@ -24,11 +27,37 @@ public class MemberService {
         return member.getId();
     }
 
+    /**
+     * 중복 아이디 검증 함수
+     */
     private void validateDuplicateMember(Member member) {
-        List<Member> result = memberRepository.findByLoginId(member.getLoginId());
-        if (!result.isEmpty()) {
+        Optional<Member> result = memberRepository.findByLoginId(member.getLoginId());
+        if (result.isPresent()) {
             throw new IllegalStateException("이미 존재하는 아이디입니다.");
         }
+    }
+
+    /**
+     * 로그인 검증 함수
+     * 로그인 아이디로 회원을 찾아서 그 회원의 비밀번호와 입력받은 비밀번호가 같은지 확인
+     * 애초에 초그인 아이디가 존재하지 않을 경우, 즉 findByLoginId의 리턴값이 null일 경우도 예외처리 해줘야 됨
+     */
+    public Member login(String loginId, String password) {
+        Optional<Member> result = validateLoginId(loginId);
+        return result
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+
+    /**
+     * 로그인 아이디 존재 여부 확인 함수
+     */
+    private Optional<Member> validateLoginId(String loginId) {
+        Optional<Member> result = memberRepository.findByLoginId(loginId);
+        if (result.isEmpty()) {
+            throw new IllegalStateException("존재하지 않는 아이디입니다.");
+        }
+        return result;
     }
 
     public Member findOne(Long id) {
@@ -39,7 +68,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public List<Member> findByLoginId(String loginId) {
+    public Optional<Member> findByLoginId(String loginId) {
         return memberRepository.findByLoginId(loginId);
     }
 }
