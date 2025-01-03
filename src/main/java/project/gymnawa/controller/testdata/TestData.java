c0dc59499f4f04c5c76d5a03b5ac0351096b6d2a
chocoding <galmeagi2@naver.com>
2025-01-03 11:54:35 +0900

    테스트 데이터 추가

--- src/main/java/project/gymnawa/controller/testdata/TestData.java
+++ src/main/java/project/gymnawa/controller/testdata/TestData.java
@@ -1,2 +1,21 @@
-package project.gymnawa.controller.testdata;public class TestData {
+package project.gymnawa.controller.testdata;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import project.gymnawa.domain.Member;
+import project.gymnawa.service.MemberService;
+
+@Component
+@RequiredArgsConstructor
+public class TestData {
+
+    private final MemberService memberService;
+
+    @PostConstruct
+    public void testDataInit() {
+        Member member = new Member("jsj121", "1234", "조성진");
+
+        memberService.join(member);
+    }
 }
