90b16e8c90d119a800267216e3c0dd04997781ee
chocoding <galmeagi2@naver.com>
2025-01-12 13:44:10 +0900

    트레이너 컨트롤러 생성

--- src/main/java/project/gymnawa/controller/TrainerController.java
+++ src/main/java/project/gymnawa/controller/TrainerController.java
@@ -1,2 +1,22 @@
-package project.gymnawa.controller;public class TrainerController {
+package project.gymnawa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import project.gymnawa.domain.dto.trainer.TrainerSaveDto;
+import project.gymnawa.service.TrainerService;
+
+@Controller
+@RequestMapping("/trainer")
+@RequiredArgsConstructor
+public class TrainerController {
+
+    private final TrainerService trainerService;
+
+    @GetMapping("/add")
+    public String createTrainerForm(@ModelAttribute TrainerSaveDto trainerSaveDto) {
+        return "/trainer/createTrainerForm";
+    }
 }
