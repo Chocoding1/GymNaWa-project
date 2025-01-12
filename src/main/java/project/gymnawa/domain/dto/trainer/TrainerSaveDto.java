01b0a45272a4dcee1e12007a460b03f7fdd31ff2
chocoding <galmeagi2@naver.com>
2025-01-12 13:44:39 +0900

    트레이너 생성 DTO 추가

--- src/main/java/project/gymnawa/domain/dto/trainer/TrainerSaveDto.java
+++ src/main/java/project/gymnawa/domain/dto/trainer/TrainerSaveDto.java
@@ -1,2 +1,17 @@
-package project.gymnawa.domain.dto.trainer;public class TrainerSaveDto {
+package project.gymnawa.domain.dto.trainer;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class TrainerSaveDto {
+
+    @NotBlank
+    private String loginId;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String name;
 }
