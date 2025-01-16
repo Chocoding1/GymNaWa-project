1d76f6bf69a56d7fafe94f96ade0ac7bb6c47d5d
chocoding <galmeagi2@naver.com>
2025-01-16 22:09:19 +0900

    email DTO 생성

---
+++ src/main/java/project/gymnawa/controller/EmailController.java
@@ -0,0 +1,2 @@
+package project.gymnawa.controller;public class Emailcontroller {
+}
---
+++ src/main/java/project/gymnawa/domain/dto/email/EmailDto.java
@@ -0,0 +1,10 @@
+package project.gymnawa.domain.dto.email;
+
+import lombok.Data;
+
+@Data
+public class EmailDto {
+
+    private String email;
+    private String code;
+}
