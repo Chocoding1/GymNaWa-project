3325f187a6881c3e6d57a8dec0fea65a3fb7aecc
chocoding <galmeagi2@naver.com>
2025-01-16 22:23:12 +0900

    redis bean 환경설정

--- src/main/java/project/gymnawa/web/config/RedisConfig.java
+++ src/main/java/project/gymnawa/web/config/RedisConfig.java
@@ -1,2 +1,31 @@
-package project.gymnawa.web.config;public class RedisConfig {
+package project.gymnawa.web.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private String port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, Integer.parseInt(port));
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
 }
