19cc5b92f394135fb1326e866de2c79e6531d52e
chocoding <galmeagi2@naver.com>
2025-01-16 22:25:09 +0900

    테스트 데이터 수정

--- src/main/java/project/gymnawa/testdata/TestData.java
+++ src/main/java/project/gymnawa/testdata/TestData.java
@@ -20,10 +20,10 @@ public class TestData {
     @PostConstruct
     public void testDataInit() {
         Address address = new Address("07809", "서울 강서구 마곡중앙1로 71", "1307동 803호", "마곡 13단지 힐스테이트 마스터");
-        NorMember normalMember = new NorMember("jsj121", "1234", "조성진", address);
+        NorMember normalMember = new NorMember("jsj121", "1234", "조성진", "whtjdwls@naver.com", address);
         norMemberService.join(normalMember);
 
-        Trainer trainer = new Trainer("jsj012100", "123456", "조성모", address);
+        Trainer trainer = new Trainer("jsj012100", "123456", "조성모", "whtjdah@gmail.com", address);
         trainerService.join(trainer);
     }
 }
