ae140b9a6a62c8b450f654925149ada37551a36e
chocoding <galmeagi2@naver.com>
2025-01-16 22:28:21 +0900

    RedisService 구현

--- src/main/java/project/gymnawa/service/RedisService.java
+++ src/main/java/project/gymnawa/service/RedisService.java
@@ -1,2 +1,35 @@
-package project.gymnawa.service;public class RedisService {
+package project.gymnawa.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public void setData(String key, String value) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, value);
+    }
+
+    public void setDAtaExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
 }
