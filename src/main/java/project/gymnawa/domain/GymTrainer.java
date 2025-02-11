eee690584310fb7ad5916db7d96ce2a841246c8e
chocoding <galmeagi2@naver.com>
2025-02-11 12:16:14 +0900

    헬스장 소속 트레이너 엔티티 추가

--- src/main/java/project/gymnawa/domain/GymTrainer.java
+++ src/main/java/project/gymnawa/domain/GymTrainer.java
@@ -1,2 +1,30 @@
-package project.gymnawa.domain;public class GymTrainer {
+package project.gymnawa.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GymTrainer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TRAINER_ID")
+    private Trainer trainer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "GYM_ID")
+    private Gym gym;
+
+    private LocalDate hireDate;
+    private LocalDate contractExpireDate;
+    private String contractActive; // 계약 상태 [유효, 만료]
 }
