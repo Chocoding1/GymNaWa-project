64bbe777956aad4b7544fb590e0990de9059460d
chocoding <galmeagi2@naver.com>
2025-02-12 22:57:15 +0900

    오류 처리 추가

--- src/main/java/project/gymnawa/service/MemberService.java
+++ src/main/java/project/gymnawa/service/MemberService.java
@@ -1,43 +1,63 @@
-//package project.gymnawa.service;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//import project.gymnawa.domain.Address;
-//import project.gymnawa.domain.Member;
-//import project.gymnawa.domain.NorMember;
-//import project.gymnawa.repository.MemberRepository;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//@Transactional(readOnly = true)
-//@RequiredArgsConstructor
-//public class MemberService {
-//
-//    private final MemberRepository memberRepository;
-//
-//    /**
-//     * 로그인
-//     * 로그인 아이디로 회원을 찾아서 그 회원의 비밀번호와 입력받은 비밀번호가 같은지 확인
-//     * 애초에 초그인 아이디가 존재하지 않을 경우, 즉 findByLoginId의 리턴값이 null일 경우도 예외처리 해줘야 됨 -> 스트림 사용하여 해결
-//     */
-//    public Member login(String loginId, String password) {
-//        return memberRepository.findByLoginId(loginId)
-//                .filter(m -> m.getPassword().equals(password))
-//                .orElse(null);
-//    }
-//
-//    public Member findOne(Long id) {
-//        return memberRepository.findOne(id);
-//    }
-//
-//    public List<Member> findMembers() {
-//        return memberRepository.findAll();
-//    }
-//
-//    public Optional<Member> findByLoginId(String loginId) {
-//        return memberRepository.findByLoginId(loginId);
-//    }
-//}
+package project.gymnawa.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.gymnawa.domain.Address;
+import project.gymnawa.domain.Member;
+import project.gymnawa.domain.NorMember;
+import project.gymnawa.repository.MemberRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 로그인
+     * 로그인 아이디로 회원을 찾아서 그 회원의 비밀번호와 입력받은 비밀번호가 같은지 확인
+     * 애초에 초그인 아이디가 존재하지 않을 경우, 즉 findByLoginId의 리턴값이 null일 경우도 예외처리 해줘야 됨 -> 스트림 사용하여 해결
+     */
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+
+    /**
+     * 회원 조회
+     */
+    public Member findOne(Long id) {
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 로그인 아이디로 회원 조회
+     */
+    public Member findByLoginId(String loginId) {
+        return memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
+    }
+
+    /**
+     * 이메일로 회원 조회
+     */
+    public Member findByEmail(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
+    }
+}
