f0f6ca3d45b5ca5345167ae6cd7c382494027bce
chocoding <galmeagi2@naver.com>
2025-02-14 15:51:42 +0900

    TrainerService 단위 테스트 완료

--- src/main/java/project/gymnawa/service/TrainerService.java
+++ src/main/java/project/gymnawa/service/TrainerService.java
@@ -1,81 +1,84 @@
-//package project.gymnawa.service;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//import project.gymnawa.domain.Address;
-//import project.gymnawa.domain.Gym;
-//import project.gymnawa.domain.Member;
-//import project.gymnawa.domain.Trainer;
-//import project.gymnawa.repository.MemberRepository;
-//import project.gymnawa.repository.TrainerRepository;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//// 서비스에서 DB에 여러 번 접근할 수 있기 때문에 서비스 단에서 한 번에 쿼리를 처리하기 위해 서비스 단에 트랜잭션을 달아준다.
-//// 디비에 여러번 접근하면서 하나의 작업을 수행하는 서비스 계층 메서드에 주로 사용 (출처: https://swmobenz.tistory.com/34 [DevYGwan:티스토리])
-//@Transactional(readOnly = true)
-//@RequiredArgsConstructor
-//public class TrainerService {
-//
-//    private final TrainerRepository trainerRepository;
-//    private final MemberRepository memberRepository;
-//
-//    /**
-//     * 회원가입
-//     */
-//    @Transactional
-//    public Long join(Trainer trainer) {
-//        validateDuplicateTrainer(trainer); // 중복 회원가입 방지
-//        trainerRepository.save(trainer);
-//        return trainer.getId();
-//    }
-//
-//    private void validateDuplicateTrainer(Trainer trainer) {
-//        Optional<Member> result = memberRepository.findByLoginId(trainer.getLoginId());
-//        if (!result.isEmpty()) {
-//            throw new IllegalStateException("이미 존재하는 아이디입니다.");
-//        }
-//    }
-//
-//    /**
-//     * 트레이너 단건 조회
-//     */
-//    public Trainer findOne(Long id) {
-//        return trainerRepository.findOne(id);
-//    }
-//
-//    /**
-//     * 이름으로 검색
-//     */
-//    public List<Trainer> findByName(String name) {
-//        return trainerRepository.findByName(name);
-//    }
-//
-//    /**
-//     * 트레이너 목록
-//     */
-//    public List<Trainer> findTrainers() {
-//        return trainerRepository.findAll();
-//    }
-//
-//    /**
-//     * 특정 헬스장 소속 트레이너 목록(이 기능을 GymService에 넣어야 할지 고민 중)
-//     *  +) GymService에는 Gym 검색 기능이 들어가기 때문에 트레이너 검색은 TrainerService에 들어가는 게 맞다고 판단
-//     */
-//    public List<Trainer> findByGym(Gym gym) {
-//        return trainerRepository.findByGym(gym);
-//    }
-//
-//    /**
-//     * 트레이너 정보 수정
-//     */
-//    @Transactional
-//    public void updateTrainer(Long id, String loginId, String password, String name, Address address) {
-//        Trainer trainer = trainerRepository.findOne(id);
-//
-//        trainer.updateInfo(loginId, password, name, address);
-//    }
-//}
+package project.gymnawa.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.gymnawa.domain.*;
+import project.gymnawa.repository.MemberRepository;
+import project.gymnawa.repository.TrainerRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+// 서비스에서 DB에 여러 번 접근할 수 있기 때문에 서비스 단에서 한 번에 쿼리를 처리하기 위해 서비스 단에 트랜잭션을 달아준다.
+// 디비에 여러번 접근하면서 하나의 작업을 수행하는 서비스 계층 메서드에 주로 사용 (출처: https://swmobenz.tistory.com/34 [DevYGwan:티스토리])
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class TrainerService {
+
+    private final TrainerRepository trainerRepository;
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Trainer trainer) {
+        validateDuplicateTrainer(trainer); // 중복 회원가입 방지
+        trainerRepository.save(trainer);
+        return trainer.getId();
+    }
+
+    private void validateDuplicateTrainer(Trainer trainer) {
+        Optional<Member> result = memberRepository.findByLoginId(trainer.getLoginId());
+        if (result.isPresent()) {
+            throw new IllegalStateException("이미 존재하는 아이디입니다.");
+        }
+    }
+
+    /**
+     * 트레이너 단건 조회
+     */
+    public Trainer findOne(Long id) {
+        return trainerRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
+    }
+
+    /**
+     * 이름으로 검색
+     */
+    public List<Trainer> findByName(String name) {
+        return trainerRepository.findByName(name);
+    }
+
+    /**
+     * 트레이너 목록
+     */
+    public List<Trainer> findTrainers() {
+        return trainerRepository.findAll();
+    }
+
+    /**
+     * 특정 헬스장 소속 트레이너 목록(이 기능을 GymService에 넣어야 할지 고민 중)
+     *  +) GymService에는 Gym 검색 기능이 들어가기 때문에 트레이너 검색은 TrainerService에 들어가는 게 맞다고 판단
+     *  +) GymTrainer 엔티티를 만들었기 때문에 이 기능은 GymTrainerService에서 진행할 예정
+     */
+/*
+    public List<Trainer> findByGym(Gym gym) {
+        return trainerRepository.findByGym(gym);
+    }
+*/
+
+    /**
+     * 트레이너 정보 수정
+     */
+    @Transactional
+    public void updateTrainer(Long id, String loginId, String password, String name, Address address, Gender gender) {
+        Trainer trainer = trainerRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
+
+        trainer.updateInfo(loginId, password, name, address, gender);
+    }
+}
--- src/test/java/project/gymnawa/service/TrainerServiceTest.java
+++ src/test/java/project/gymnawa/service/TrainerServiceTest.java
@@ -1,119 +1,207 @@
-//package project.gymnawa.service;
-//
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//import project.gymnawa.domain.Address;
-//import project.gymnawa.domain.Gym;
-//import project.gymnawa.domain.Trainer;
-//
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//@Transactional
-//class TrainerServiceTest {
-//
-//    @Autowired
-//    private TrainerService trainerService;
-//    @Autowired
-//    private GymService gymService;
-//
-//    @Test
-//    void join() {
-//        //given
-//        Trainer trainer = new Trainer("jsj012100", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//
-//        //when
-//        Long joinId = trainerService.join(trainer);
-//
-//        //then
-//        assertThat(joinId).isEqualTo(trainer.getId());
-//    }
-//
-//    @Test
-//    public void 중복_트레이너_테스트() {
-//        //given
-//        Trainer trainer1 = new Trainer("jsj012100", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//        Trainer trainer2 = new Trainer("jsj012100", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//
-//        //when
-//        trainerService.join(trainer1);
-//
-//        //then
-//        assertThrows(IllegalStateException.class,
-//                () -> trainerService.join(trainer2));
-//    }
-//
-//    @Test
-//    void findByName() {
-//        //given
-//        Trainer trainer1 = new Trainer("jsj012100", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//        Trainer trainer2 = new Trainer("jsj0121", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//        Trainer trainer3 = new Trainer("jsj121", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//
-//        trainerService.join(trainer1);
-//        trainerService.join(trainer2);
-//        trainerService.join(trainer3);
-//
-//        //when
-//        List<Trainer> result = trainerService.findByName("조성진");
-//
-//        //then
-//        assertThat(result.size()).isEqualTo(2);
-//    }
-//
-//    @Test
-//    void findTrainers() {
-//        //given
-//        Trainer trainer1 = new Trainer("jsj012100", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//        Trainer trainer2 = new Trainer("jsj0121", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//        Trainer trainer3 = new Trainer("jsj121", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//
-//        trainerService.join(trainer1);
-//        trainerService.join(trainer2);
-//        trainerService.join(trainer3);
-//
-//        //when
-//        List<Trainer> result = trainerService.findTrainers();
-//
-//        //then
-//        assertThat(result.size()).isEqualTo(3);
-//        assertThat(result).contains(trainer1, trainer2, trainer3);
-//    }
-//
-//    /**
-//     * Gym 리포지토리와 서비스를 만들어야 테스트 가능하기 때문에 일단 보류
-//     */
-//    @Test
-//    void findTrainersByGym() {
-//        //given
-//        Trainer trainer1 = new Trainer("jsj012100", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//        Trainer trainer2 = new Trainer("jsj0121", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//        Trainer trainer3 = new Trainer("jsj012100", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//
-//        Gym gym = new Gym("gym1");
-//        gymService.join(gym);
-//        System.out.println("gym.getId() = " + gym.getId());
-//
-//        trainer1.changeGym(gym);
-//        trainer3.changeGym(gym);
-//
-//        trainerService.join(trainer1);
-//        trainerService.join(trainer2);
-//        trainerService.join(trainer3);
-//
-//        //when
-//        List<Trainer> result = trainerService.findByGym(gym);
-//
-//        //then
-//        assertThat(result.size()).isEqualTo(2);
-//        assertThat(result).contains(trainer1, trainer3);
-//    }
-//
-//}
+package project.gymnawa.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import project.gymnawa.domain.*;
+import project.gymnawa.repository.MemberRepository;
+import project.gymnawa.repository.TrainerRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.inOrder;
+
+@ExtendWith(MockitoExtension.class)
+class TrainerServiceTest {
+
+    @InjectMocks
+    TrainerService trainerService;
+
+    @Mock
+    TrainerRepository trainerRepository;
+    @Mock
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void joinSuccess() {
+        //given
+        Trainer trainer = createTrainer("jsj012100", "1234", "조성진");
+
+        when(trainerRepository.save(trainer)).thenReturn(trainer);
+        when(memberRepository.findByLoginId("jsj012100")).thenReturn(Optional.empty());
+
+        //when
+        Long joinId = trainerService.join(trainer);
+
+        //then
+        assertThat(joinId).isEqualTo(trainer.getId());
+        verify(memberRepository, times(1)).findByLoginId("jsj012100");
+        verify(trainerRepository, times(1)).save(trainer);
+
+        InOrder inOrder = inOrder(memberRepository, trainerRepository);
+        inOrder.verify(memberRepository).findByLoginId("jsj012100");
+        inOrder.verify(trainerRepository).save(trainer);
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 중복 아이디는 입력 불가")
+    void joinFail() {
+        //given
+        Trainer trainer = createTrainer("jsj012100", "aadfad", "조성진");
+        Trainer dupliTrainer = createTrainer("jsj012100", "aadfad", "조성진");
+
+        when(memberRepository.findByLoginId("jsj012100")).thenReturn(Optional.of(trainer));
+
+        //when & then
+        assertThrows(IllegalStateException.class,
+                () -> trainerService.join(dupliTrainer));
+        verify(memberRepository, times(1)).findByLoginId("jsj012100");
+    }
+
+    @Test
+    @DisplayName("트레이너 조회 성공")
+    void findTrainerSuccess() {
+        //given
+        Trainer trainer = Trainer.builder()
+                .id(1L)
+                .loginId("jsj012100")
+                .password("1234")
+                .name("조성진")
+                .email("galmeagi2@naver.com")
+                .gender(Gender.MALE)
+                .build();
+
+        when(trainerRepository.findById(1L)).thenReturn(Optional.of(trainer));
+
+        //when
+        Trainer findTrainer = trainerService.findOne(1L);
+
+        //then
+        assertThat(findTrainer.getEmail()).isEqualTo(trainer.getEmail());
+        verify(trainerRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("회원 조회 실패 - 존재하지 않는 회원")
+    void findTrainerFail() {
+        //given
+        Long id = 1L;
+        when(trainerRepository.findById(id)).thenReturn(Optional.empty());
+
+        //when & then
+        assertThrows(NoSuchElementException.class,
+                () -> trainerService.findOne(id));
+
+        verify(trainerRepository, times(1)).findById(id);
+    }
+
+    @Test
+    @DisplayName("트레이너 이름으로 조회")
+    void findByName() {
+        //given
+        Trainer trainer1 = createTrainer("jsj012100", "aadfad", "조성진");
+        Trainer trainer2 = createTrainer("jsj0121", "aadfad", "조성진");
+        Trainer trainer3 = createTrainer("jsj121", "aadfad", "조성진");
+
+        List<Trainer> trainers = Arrays.asList(trainer1, trainer2, trainer3);
+
+        when(trainerRepository.findByName("조성진")).thenReturn(trainers);
+
+        //when
+        List<Trainer> result = trainerService.findByName("조성진");
+
+        //then
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result).contains(trainer1, trainer2, trainer3);
+
+        verify(trainerRepository, times(1)).findByName("조성진");
+    }
+
+    @Test
+    @DisplayName("트레이너 목록")
+    void findTrainers() {
+        //given
+        Trainer trainer1 = createTrainer("jsj012100", "aadfad", "조성진");
+        Trainer trainer2 = createTrainer("jsj0121", "aadfad", "조성진");
+        Trainer trainer3 = createTrainer("jsj121", "aadfad", "조성진");
+
+        List<Trainer> trainers = Arrays.asList(trainer1, trainer2, trainer3);
+
+        when(trainerRepository.findAll()).thenReturn(trainers);
+
+        //when
+        List<Trainer> result = trainerService.findTrainers();
+
+        //then
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result).contains(trainer1, trainer2, trainer3);
+
+        verify(trainerRepository, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("트레이너 정보 수정 성공")
+    void updateTrainerSuccess() {
+        //given
+        Trainer trainer = Trainer.builder()
+                .id(1L)
+                .loginId("oldLoginId")
+                .password("oldPw")
+                .name("oldName")
+                .email("oldMail")
+                .address(new Address("oldZone", "oldAddress", "oldDetail", "oldBuilding"))
+                .gender(Gender.MALE)
+                .build();
+
+        when(trainerRepository.findById(1L)).thenReturn(Optional.of(trainer));
+
+        //when
+        trainerService.updateTrainer(1L, "newLoginId", "newPw", "newName", new Address("newZone", "newAddress", "newDetail", "newBuilding"), Gender.FEMALE);
+
+        //then
+        assertThat(trainer.getLoginId()).isEqualTo("newLoginId");
+        assertThat(trainer.getPassword()).isEqualTo("newPw");
+        assertThat(trainer.getName()).isEqualTo("newName");
+        assertThat(trainer.getAddress().getZoneCode()).isEqualTo("newZone");
+        assertThat(trainer.getGender()).isEqualTo(Gender.FEMALE);
+
+        verify(trainerRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("회원 정보 수정 실패 - 존재하지 않는 회원")
+    void updateMemberFail() {
+        //given
+        when(trainerRepository.findById(1L)).thenReturn(Optional.empty());
+
+        //when & then
+        assertThrows(NoSuchElementException.class,
+                () -> trainerService.updateTrainer(1L, "newLoginId", "newPw", "newName", new Address("newZone", "newAddress", "newDetail", "newBuilding"), Gender.FEMALE));
+
+        verify(trainerRepository, times(1)).findById(1L);
+    }
+
+    private Trainer createTrainer(String loginId, String password, String name) {
+        return Trainer.builder()
+                .loginId(loginId)
+                .password(password)
+                .name(name)
+                .build();
+    }
+}
