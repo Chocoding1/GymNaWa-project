5c3ec7b29be7985d30ca38b0c3ae44bcd9334b2e
chocoding <galmeagi2@naver.com>
2025-02-14 12:50:24 +0900

    NorMemberService 단위 테스트 완료

--- src/test/java/project/gymnawa/service/NorMemberServiceTest.java
+++ src/test/java/project/gymnawa/service/NorMemberServiceTest.java
@@ -1,73 +1,164 @@
-//package project.gymnawa.service;
-//
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.transaction.annotation.Transactional;
-//import project.gymnawa.domain.Address;
-//import project.gymnawa.domain.Gender;
-//import project.gymnawa.domain.Member;
-//import project.gymnawa.domain.NorMember;
-//import project.gymnawa.repository.MemberRepository;
-//import project.gymnawa.repository.NorMemberRepository;
-//
-//import static org.assertj.core.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//class NorMemberServiceTest {
-//
-//    @InjectMocks
-//    private NorMemberService norMemberService;
-//
-//    @Mock
-//    private NorMemberRepository norMemberRepository;
-//    @Mock
-//    private MemberRepository memberRepository;
-//
-//    @Test
-//    @DisplayName("회원가입 성공")
-//    void join() {
-//        //given
-//        Address address = new Address("12345", "서울", "강서구", "마곡동");
-//        NorMember norMember = new NorMember("jsj012100", "aadfad", "조성진", "galmeagi2@naver.com", address, Gender.MALE);
-//
-//        Mockito.when(norMemberRepository.save(norMember)).thenReturn(norMember);
-//
-//        //when
-//        norMemberService.join(norMember);
-//
-//        //then
-//        assertThat(findMember).isSameAs(norMember);
-//        assertThat(joinId).isEqualTo(norMember.getId());
-//    }
-//
-//    @DisplayName("회원가입 시 중복 아이디는 입력 불가")
-//    @Test
-//    void join_duplicate_loginId() {
-//        //given
-//        NorMember norMember = new NorMember("jsj012100", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//        NorMember dupliMember = new NorMember("jsj012100", "aadfad", "조성진", new Address("서울", "강서구", "마곡동", "힐스테이트"));
-//
-//        //when
-//        Long joinId = norMemberService.join(norMember);
-//
-//
-//        //then
-//        assertThrows(IllegalStateException.class,
-//                () -> norMemberService.join(dupliMember));
-//    }
-//
-//    @Test
-//    void updateMember() {
-//    }
-//
-//    @Test
-//    void findOne() {
-//    }
-//}
+package project.gymnawa.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import project.gymnawa.domain.Address;
+import project.gymnawa.domain.Gender;
+import project.gymnawa.domain.NorMember;
+import project.gymnawa.repository.MemberRepository;
+import project.gymnawa.repository.NorMemberRepository;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class NorMemberServiceTest {
+
+    @InjectMocks
+    private NorMemberService norMemberService;
+
+    @Mock
+    private NorMemberRepository norMemberRepository;
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join() {
+        //given
+        Address address = new Address("12345", "서울", "강서구", "마곡동");
+        NorMember norMember = NorMember.builder()
+                .id(1L)
+                .loginId("jsj012100")
+                .password("1234")
+                .name("조성진")
+                .email("galmeagi2@naver.com")
+                .address(address)
+                .gender(Gender.MALE)
+                .build();
+
+        when(norMemberRepository.save(norMember)).thenReturn(norMember);
+        when(memberRepository.findByLoginId("jsj012100")).thenReturn(Optional.empty());
+
+        //when
+        Long joinId = norMemberService.join(norMember);
+
+        //then
+        assertThat(joinId).isEqualTo(norMember.getId());
+        verify(memberRepository, times(1)).findByLoginId("jsj012100");
+        verify(norMemberRepository, times(1)).save(norMember);
+
+        InOrder inOrder = inOrder(memberRepository, norMemberRepository);
+        inOrder.verify(memberRepository).findByLoginId("jsj012100");
+        inOrder.verify(norMemberRepository).save(norMember);
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - 중복 아이디는 입력 불가")
+    void join_duplicate_loginId() {
+        //given
+        NorMember norMember = createNorMember("jsj012100", "aadfad", "조성진");
+        NorMember dupliMember = createNorMember("jsj012100", "aadfad", "조성진");
+
+        when(memberRepository.findByLoginId("jsj012100")).thenReturn(Optional.of(norMember));
+
+        //when & then
+        assertThrows(IllegalStateException.class,
+                () -> norMemberService.join(dupliMember));
+        verify(memberRepository, times(1)).findByLoginId("jsj012100");
+    }
+
+    @Test
+    @DisplayName("회원 정보 수정 성공")
+    void updateMemberSuccess() {
+        //given
+        NorMember norMember = NorMember.builder()
+                .id(1L)
+                .loginId("oldLoginId")
+                .password("oldPw")
+                .name("oldName")
+                .email("oldMail")
+                .address(new Address("oldZone", "oldAddress", "oldDetail", "oldBuilding"))
+                .gender(Gender.MALE)
+                .build();
+
+        when(norMemberRepository.findById(1L)).thenReturn(Optional.of(norMember));
+
+        //when
+        norMemberService.updateMember(1L, "newLoginId", "newPw", "newName", new Address("newZone", "newAddress", "newDetail", "newBuilding"), Gender.FEMALE);
+
+        //then
+        assertThat(norMember.getLoginId()).isEqualTo("newLoginId");
+        assertThat(norMember.getPassword()).isEqualTo("newPw");
+        assertThat(norMember.getName()).isEqualTo("newName");
+        assertThat(norMember.getAddress().getZoneCode()).isEqualTo("newZone");
+        assertThat(norMember.getGender()).isEqualTo(Gender.FEMALE);
+
+        verify(norMemberRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("회원 정보 수정 실패 - 존재하지 않는 회원")
+    void updateMemberFail() {
+        //given
+        when(norMemberRepository.findById(1L)).thenReturn(Optional.empty());
+
+        //when & then
+        assertThrows(NoSuchElementException.class,
+                () -> norMemberService.updateMember(1L, "newLoginId", "newPw", "newName", new Address("newZone", "newAddress", "newDetail", "newBuilding"), Gender.FEMALE));
+
+        verify(norMemberRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("회원 조회 성공")
+    void findNorMemberSuccess() {
+        //given
+        NorMember norMember = NorMember.builder()
+                .id(1L)
+                .loginId("jsj012100")
+                .password("1234")
+                .email("galmeagi2@naver.com")
+                .build();
+
+        when(norMemberRepository.findById(1L)).thenReturn(Optional.of(norMember));
+
+        //when
+        NorMember findNorMember = norMemberService.findOne(1L);
+
+        //then
+        assertThat(findNorMember.getEmail()).isEqualTo(norMember.getEmail());
+        verify(norMemberRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("회원 조회 실패 - 존재하지 않는 회원")
+    void findNorMemberFail() {
+        //given
+        Long id = 1L;
+        when(norMemberRepository.findById(id)).thenReturn(Optional.empty());
+
+        //when & then
+        assertThrows(NoSuchElementException.class,
+                () -> norMemberService.findOne(id));
+
+        verify(norMemberRepository, times(1)).findById(id);
+    }
+
+    private NorMember createNorMember(String loginId, String password, String name) {
+        return NorMember.builder()
+                .loginId(loginId)
+                .password(password)
+                .name(name)
+                .build();
+    }
+}
