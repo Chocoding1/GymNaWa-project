b6db0ee850789dcac0cddaf6a31e3a5b2c5bbaae
chocoding <galmeagi2@naver.com>
2025-02-14 16:43:34 +0900

    Builder 패턴 적용

--- src/main/java/project/gymnawa/domain/Gym.java
+++ src/main/java/project/gymnawa/domain/Gym.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,7 +24,9 @@ public class Gym {
     @Embedded
     private Address address;
 
-    public Gym(String storeName, String storePhone, Address address, String runday, String runtime) {
+    @Builder
+    public Gym(Long id, String storeName, String storePhone, Address address, String runday, String runtime) {
+        this.id = id;
         this.storeName = storeName;
         this.storePhone = storePhone;
         this.address = address;
