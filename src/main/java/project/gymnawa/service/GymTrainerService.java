ca6aba2aa2ec520156852847209654fcc8a0a21a
chocoding <galmeagi2@naver.com>
2025-02-17 12:08:50 +0900

    GymTrainerService 단위 테스트 완료

--- src/main/java/project/gymnawa/service/GymTrainerService.java
+++ src/main/java/project/gymnawa/service/GymTrainerService.java
@@ -1,2 +1,43 @@
-package project.gymnawa.service;public class GymTrainerService {
+package project.gymnawa.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.gymnawa.domain.ContractStatus;
+import project.gymnawa.domain.Gym;
+import project.gymnawa.domain.GymTrainer;
+import project.gymnawa.domain.Trainer;
+import project.gymnawa.repository.GymTrainerRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class GymTrainerService {
+
+    private final GymTrainerRepository gymTrainerRepository;
+
+    /**
+     * 트레이너 계약 정보 저장
+     */
+    public Long save(GymTrainer gymTrainer) {
+        gymTrainerRepository.save(gymTrainer);
+        return gymTrainer.getId();
+    }
+
+    /**
+     * 특정 트레이너의 계약 정보 조회
+     */
+    public List<GymTrainer> findByTrainer(Trainer trainer) {
+        return gymTrainerRepository.findByTrainer(trainer);
+    }
+
+    /**
+     * 헬스장과 계약 상태 별 계약 정보 조회
+     * 특정 헬스장에 근무하는 트레이너 조회용 메서드
+     */
+    public List<GymTrainer> findByGymAndContractStatus(Gym gym, ContractStatus contractStatus) {
+        return gymTrainerRepository.findByGymAndContractStatus(gym, contractStatus);
+    }
 }
--- src/test/java/project/gymnawa/service/GymTrainerServiceTest.java
+++ src/test/java/project/gymnawa/service/GymTrainerServiceTest.java
@@ -1,4 +1,121 @@
-import static org.junit.jupiter.api.Assertions.*;
+package project.gymnawa.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import project.gymnawa.domain.ContractStatus;
+import project.gymnawa.domain.Gym;
+import project.gymnawa.domain.GymTrainer;
+import project.gymnawa.domain.Trainer;
+import project.gymnawa.repository.GymTrainerRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class GymTrainerServiceTest {
-  
+
+    @InjectMocks
+    GymTrainerService gymTrainerService;
+
+    @Mock
+    GymTrainerRepository gymTrainerRepository;
+
+    @Test
+    @DisplayName("트레이너 계약 정보 저장")
+    void save() {
+        //given
+        GymTrainer gymTrainer = GymTrainer.builder()
+                .id(1L)
+                .gym(Gym.builder().build())
+                .trainer(Trainer.builder().build())
+                .contractStatus(ContractStatus.ACTIVE)
+                .build();
+
+        //when
+        Long savedId = gymTrainerService.save(gymTrainer);
+
+        //then
+        assertThat(savedId).isEqualTo(1L);
+
+        verify(gymTrainerRepository, times(1)).save(gymTrainer);
+    }
+
+    @Test
+    @DisplayName("트레이너별 계약 정보 조회")
+    void findByTrainer() {
+        //given
+        Trainer trainer = createTrainer("jsj012100", "1234", "조성진", "galmeagi2@naver.com");
+        Gym gym = createGym("라온짐");
+        GymTrainer gymTrainer1 = createGymTrainer(trainer, gym, ContractStatus.ACTIVE);
+        GymTrainer gymTrainer2 = createGymTrainer(trainer, gym, ContractStatus.EXPIRED);
+
+        List<GymTrainer> gymTrainers = Arrays.asList(gymTrainer1, gymTrainer2);
+
+        when(gymTrainerRepository.findByTrainer(trainer)).thenReturn(gymTrainers);
+
+        //when
+        List<GymTrainer> result = gymTrainerService.findByTrainer(trainer);
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+
+        verify(gymTrainerRepository, times(1)).findByTrainer(trainer);
+    }
+
+    @Test
+    @DisplayName("헬스장과 계약 상태 별 계약 정보 조회")
+    void findByGymAndContractStatus() {
+        //given
+        Trainer trainer1 = createTrainer("jsj012100", "1234", "조성진", "galmeagi2@naver.com");
+        Trainer trainer2 = createTrainer("jsj121", "12345", "조성민", "galmeagi2@naver.com");
+        Gym gym = createGym("라온짐");
+
+        GymTrainer gymTrainer1 = createGymTrainer(trainer1, gym, ContractStatus.ACTIVE);
+        GymTrainer gymTrainer2 = createGymTrainer(trainer1, gym, ContractStatus.EXPIRED);
+        GymTrainer gymTrainer3 = createGymTrainer(trainer2, gym, ContractStatus.ACTIVE);
+
+        List<GymTrainer> gymTrainers = Arrays.asList(gymTrainer1, gymTrainer3);
+
+        when(gymTrainerRepository.findByGymAndContractStatus(gym, ContractStatus.ACTIVE)).thenReturn(gymTrainers);
+
+        //when
+        List<GymTrainer> result = gymTrainerService.findByGymAndContractStatus(gym, ContractStatus.ACTIVE);
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+
+        verify(gymTrainerRepository, times(1)).findByGymAndContractStatus(gym, ContractStatus.ACTIVE);
+    }
+
+    private static GymTrainer createGymTrainer(Trainer trainer, Gym gym, ContractStatus contractStatus) {
+        return GymTrainer.builder()
+                .gym(gym)
+                .trainer(trainer)
+                .contractStatus(contractStatus)
+                .build();
+    }
+
+    private Gym createGym(String storeName) {
+        return Gym.builder()
+                .storeName(storeName)
+                .build();
+    }
+
+    private Trainer createTrainer(String loginId, String password, String name, String email) {
+        return Trainer.builder()
+                .loginId(loginId)
+                .password(password)
+                .name(name)
+                .email(email)
+                .build();
+    }
 }
