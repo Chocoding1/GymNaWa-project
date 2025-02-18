53d50f8c0f84bb0fd48956fb56bb51fe8f8fddc7
chocoding <galmeagi2@naver.com>
2025-02-18 22:00:31 +0900

    로그인 성공 단위 테스트 완료

--- src/main/java/project/gymnawa/controller/api/MemberApiController.java
+++ src/main/java/project/gymnawa/controller/api/MemberApiController.java
@@ -1,8 +1,15 @@
 package project.gymnawa.controller.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.gymnawa.domain.Member;
+import project.gymnawa.domain.dto.member.MemberLoginDto;
 import project.gymnawa.service.MemberService;
 
 @RestController
@@ -11,4 +18,11 @@ import project.gymnawa.service.MemberService;
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody MemberLoginDto memberLoginDto) {
+        Member loginedMember = memberService.login(memberLoginDto.getLoginId(), memberLoginDto.getPassword());
+
+        return ResponseEntity.status(HttpStatus.OK).body("login succesful");
+    }
 }
--- src/test/java/project/gymnawa/controller/api/MemberApiControllerTest.java
+++ src/test/java/project/gymnawa/controller/api/MemberApiControllerTest.java
@@ -1,4 +1,83 @@
+package project.gymnawa.controller.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import project.gymnawa.domain.Gender;
+import project.gymnawa.domain.Member;
+import project.gymnawa.domain.dto.member.MemberLoginDto;
+import project.gymnawa.service.MemberService;
+
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(MemberApiController.class)
+@ExtendWith(MockitoExtension.class)
 class MemberApiControllerTest {
-  
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    /**
+     * Spring Boot 3.4.0부터 @MockBean은 Deprecated
+     * 대신 @MockitoBean으로 대체되었다.
+     * https://hdbstn3055.tistory.com/336
+     * @WebMvcTest + @MockitoBean 사용
+     */
+    @MockitoBean
+    private MemberService memberService;
+
+    @Test
+    @DisplayName("로그인 성공")
+    void saveSuccess() throws Exception {
+        //given
+        String loginId = "jsj012100";
+        String password = "1234";
+
+        Member loginedMember = Member.builder()
+                .id(1L)
+                .loginId(loginId)
+                .password(password)
+                .name("조성진")
+                .email("galmeagi2@naver.com")
+                .gender(Gender.MALE)
+                .build();
+
+        MemberLoginDto memberLoginDto = MemberLoginDto.builder()
+                .loginId(loginId)
+                .password(password)
+                .build();
+
+        when(memberService.login(loginId, password)).thenReturn(loginedMember);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/api/member/login")
+                .content(objectMapper.writeValueAsString(memberLoginDto))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        resultActions
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 }
