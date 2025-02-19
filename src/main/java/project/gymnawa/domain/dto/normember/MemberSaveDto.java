523a41aa27902c7ccb6731da1432d0b06b1c3ea6
chocoding <galmeagi2@naver.com>
2025-02-19 17:08:29 +0900

    회원가입 DTO Builder 패턴 적용 & 필드 추가

--- src/main/java/project/gymnawa/domain/dto/normember/MemberSaveDto.java
+++ src/main/java/project/gymnawa/domain/dto/normember/MemberSaveDto.java
@@ -1,9 +1,13 @@
 package project.gymnawa.domain.dto.normember;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import project.gymnawa.domain.Gender;
 
 @Data
+@NoArgsConstructor
 public class MemberSaveDto {
 
     @NotBlank
@@ -19,19 +23,22 @@ public class MemberSaveDto {
     private String email;
 
     @NotBlank
+    private Gender gender;
+
+    @NotBlank
     private String zoneCode;
     @NotBlank
     private String address;
     private String detailAddress;
     private String buildingName;
 
-    public MemberSaveDto() {
-    }
-
-    public MemberSaveDto(String loginId, String password, String name, String zoneCode, String address, String detailAddress, String buildingName) {
+    @Builder
+    public MemberSaveDto(String loginId, String password, String name, String email, Gender gender, String zoneCode, String address, String detailAddress, String buildingName) {
         this.loginId = loginId;
         this.password = password;
         this.name = name;
+        this.email = email;
+        this.gender = gender;
         this.zoneCode = zoneCode;
         this.address = address;
         this.detailAddress = detailAddress;
