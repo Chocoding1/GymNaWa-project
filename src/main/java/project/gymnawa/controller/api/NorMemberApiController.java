34388858f8e6b9e6b3a08b8e85c847a04bc9ee59
chocoding <galmeagi2@naver.com>
2025-02-19 17:08:47 +0900

    NorMemberApiController 단위 테스트 진행 중

--- src/main/java/project/gymnawa/controller/api/NorMemberApiController.java
+++ src/main/java/project/gymnawa/controller/api/NorMemberApiController.java
@@ -1,2 +1,39 @@
-package project.gymnawa.controller.api;public class NorMemberApiController {
+package project.gymnawa.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.gymnawa.domain.Gender;
+import project.gymnawa.domain.dto.normember.MemberSaveDto;
+import project.gymnawa.service.EmailService;
+import project.gymnawa.service.NorMemberService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member/n")
+@Slf4j
+public class NorMemberApiController {
+
+    private final NorMemberService norMemberService;
+    private final EmailService emailService;
+
+    @GetMapping("/add")
+    public ResponseEntity<MemberSaveDto> addForm() {
+        MemberSaveDto memberSaveDto = MemberSaveDto.builder()
+                .loginId("")
+                .password("")
+                .name("")
+                .email("")
+                .gender(Gender.MALE)
+                .zoneCode("")
+                .address("")
+                .detailAddress("")
+                .buildingName("")
+                .build();
+
+        return ResponseEntity.ok().body(memberSaveDto);
+    }
 }
--- src/test/java/project/gymnawa/controller/api/NorMemberApiControllerTest.java
+++ src/test/java/project/gymnawa/controller/api/NorMemberApiControllerTest.java
@@ -1,4 +1,55 @@
+package project.gymnawa.controller.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import project.gymnawa.domain.Gender;
+import project.gymnawa.service.EmailService;
+import project.gymnawa.service.NorMemberService;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(NorMemberApiController.class)
+@ExtendWith(MockitoExtension.class)
 class NorMemberApiControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private NorMemberService norMemberService;
+
+    @MockitoBean
+    private EmailService emailService;
+
+    @Test
+    @DisplayName("회원가입 시 초기 DTO 생성")
+    void createAddDto() throws Exception {
+        //given
+        String loginId = "";
+        String password = "";
+        Gender gender = Gender.MALE;
+
+        //when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/member/n/add"));
+
+        //then
+        resultActions
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.loginId").value(loginId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password").value(password));
+    }
 }
