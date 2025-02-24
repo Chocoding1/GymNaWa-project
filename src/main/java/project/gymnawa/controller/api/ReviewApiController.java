9e9e851165adeaf9199ec856f8cfbbf13e9c383b
chocoding <galmeagi2@naver.com>
2025-02-24 15:48:18 +0900

    Controller 진행 중

--- src/main/java/project/gymnawa/controller/api/ReviewApiController.java
+++ src/main/java/project/gymnawa/controller/api/ReviewApiController.java
@@ -7,8 +7,12 @@
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.SessionAttribute;
+//import project.gymnawa.domain.Member;
+//import project.gymnawa.domain.NorMember;
+//import project.gymnawa.domain.Review;
 //import project.gymnawa.domain.api.ApiResponse;
 //import project.gymnawa.service.ReviewService;
+//import project.gymnawa.web.SessionConst;
 //
 //@RestController
 //@Slf4j
@@ -18,8 +22,22 @@
 //
 //    private final ReviewService reviewService;
 //
+//    /**
+//     * 리뷰 추가
+//     */
 //    @GetMapping("/add")
-//    public ResponseEntity<ApiResponse<?>> reviewForm(@SessionAttribute) {
+//    public ResponseEntity<ApiResponse<?>> reviewForm(@SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) NorMember loginedNorMember) {
+//        Review.builder()
+//                .norMember(loginedNorMember)
+//                .trainer()
 //
 //    }
+//
+//    /**
+//     * 리뷰 수정
+//     */
+//
+//    /**
+//     * 리뷰 삭제
+//     */
 //}
