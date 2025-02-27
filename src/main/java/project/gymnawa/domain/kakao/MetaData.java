e6b442660ee0f12e92dfe802faa68e2700e1208d
chocoding <galmeagi2@naver.com>
2025-02-27 11:19:20 +0900

    카카오 로컬 api 메타값 받는 객체

--- src/main/java/project/gymnawa/domain/kakao/MetaData.java
+++ src/main/java/project/gymnawa/domain/kakao/MetaData.java
@@ -1,2 +1,17 @@
-package project.gymnawa.domain.kakao;public class MetaData {
+package project.gymnawa.domain.kakao;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class MetaData {
+
+    @JsonProperty("total_count")
+    private int totalCount;
+
+    @JsonProperty("pageable_count")
+    private int pageableCount;
+
+    @JsonProperty("is_end")
+    private boolean isEnd;
 }
