544f01441a5532f4d86669c0e502d194fd2ad1ad
chocoding <galmeagi2@naver.com>
2025-03-06 00:39:24 +0900

    필드 수정

--- src/main/java/project/gymnawa/domain/GymTrainer.java
+++ src/main/java/project/gymnawa/domain/GymTrainer.java
@@ -21,23 +21,19 @@ public class GymTrainer {
     @JoinColumn(name = "TRAINER_ID")
     private Trainer trainer;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "GYM_ID")
-    private Gym gym;
+    private String gymId;
 
     private LocalDate hireDate;
-    private LocalDate expireDate;
 
     @Enumerated(value = EnumType.STRING)
     private ContractStatus contractStatus; // 계약 상태 [유효, 만료]
 
     @Builder
-    public GymTrainer(Long id, Trainer trainer, Gym gym, LocalDate hireDate, LocalDate expireDate, ContractStatus contractStatus) {
+    public GymTrainer(Long id, Trainer trainer, String gymId, LocalDate hireDate, ContractStatus contractStatus) {
         this.id = id;
         this.trainer = trainer;
-        this.gym = gym;
+        this.gymId = gymId;
         this.hireDate = hireDate;
-        this.expireDate = expireDate;
         this.contractStatus = contractStatus;
     }
 }
