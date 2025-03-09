3c9c379e294d709a69294d6c3135285afb743e75
chocoding <galmeagi2@naver.com>
2025-03-09 20:46:16 +0900

    어노테이션 추가

--- src/main/java/project/gymnawa/domain/Gender.java
+++ src/main/java/project/gymnawa/domain/Gender.java
@@ -1,15 +1,13 @@
 package project.gymnawa.domain;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum Gender {
     MALE("남"),
     FEMALE("여");
 
     private final String exp;
-
-    private Gender(String exp) {
-        this.exp = exp;
-    }
 }
