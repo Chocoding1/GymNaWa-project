b09db8b71b73166fb87a97457390b83d030e1b3e
chocoding <galmeagi2@naver.com>
2025-03-11 21:24:28 +0900

    진행 중인 PT 화면 생성

---
+++ src/main/java/project/gymnawa/controller/view/PtMembershipController.java
@@ -0,0 +1,2 @@
+package project.gymnawa.controller.view;public class PtMembershipController {
+}
---
+++ src/main/java/project/gymnawa/domain/dto/ptmembership/PtMembershipViewDto.java
@@ -0,0 +1,2 @@
+package project.gymnawa.domain.dto.ptmembership;public class PtMembershipViewDto {
+}
