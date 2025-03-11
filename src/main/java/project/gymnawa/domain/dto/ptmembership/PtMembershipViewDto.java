c760a9efefb7d47f5d7b12359db2c43d17bb3a86
chocoding <galmeagi2@naver.com>
2025-03-11 21:26:56 +0900

    PT 멤버쉽 DTO 생성

--- src/main/java/project/gymnawa/domain/dto/ptmembership/PtMembershipViewDto.java
+++ src/main/java/project/gymnawa/domain/dto/ptmembership/PtMembershipViewDto.java
@@ -1,2 +1,21 @@
-package project.gymnawa.domain.dto.ptmembership;public class PtMembershipViewDto {
+package project.gymnawa.domain.dto.ptmembership;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class PtMembershipViewDto {
+
+    private String memberName;
+
+    private Long trainerId;
+
+    private String trainerName;
+
+    private int initCount;
+
+    private int remainCount;
 }
