1cdb266b16cf34f777b06a4d76f4d0da67e3de69
chocoding <galmeagi2@naver.com>
2025-03-13 01:11:22 +0900

    PT 등록 DTO 생성

--- src/main/java/project/gymnawa/domain/dto/ptmembership/PtMembershipSaveDto.java
+++ src/main/java/project/gymnawa/domain/dto/ptmembership/PtMembershipSaveDto.java
@@ -1,2 +1,21 @@
-package project.gymnawa.domain.dto.ptmembership;public class PtMembershipSaveDto {
+package project.gymnawa.domain.dto.ptmembership;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class PtMembershipSaveDto {
+
+    @NotNull
+    private Long trainerId;
+
+    @NotNull
+    private int initCount;
+
+    @NotNull
+    private int price;
 }
