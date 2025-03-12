51bf494ee2ce45c3b81355d5bc76279dc4a1d7ba
chocoding <galmeagi2@naver.com>
2025-03-13 01:11:47 +0900

    PT 등록 API 완성

--- src/main/java/project/gymnawa/controller/api/PtMembershipApiController.java
+++ src/main/java/project/gymnawa/controller/api/PtMembershipApiController.java
@@ -1,2 +1,80 @@
-package project.gymnawa.controller.api;public class PtMembershipApiController {
+package project.gymnawa.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import project.gymnawa.domain.api.ApiResponse;
+import project.gymnawa.domain.dto.ptmembership.PtMembershipSaveDto;
+import project.gymnawa.domain.dto.ptmembership.PtMembershipViewDto;
+import project.gymnawa.domain.entity.NorMember;
+import project.gymnawa.domain.entity.PtMembership;
+import project.gymnawa.domain.entity.Trainer;
+import project.gymnawa.service.PtMembershipService;
+import project.gymnawa.service.TrainerService;
+import project.gymnawa.web.SessionConst;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/ptmembership")
+@RequiredArgsConstructor
+public class PtMembershipApiController {
+
+    private final PtMembershipService ptMembershipService;
+    private final TrainerService trainerService;
+
+    /**
+     * PT 등록
+     */
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<Long>> registerPt(
+            @SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) NorMember loginedMember,
+            @Validated @RequestBody PtMembershipSaveDto ptMembershipSaveDto,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            log.info("errors = " + bindingResult);
+            return ResponseEntity.badRequest().body(ApiResponse.error("필수 입력사항을 모두 입력해주세요."));
+        }
+
+        Trainer trainer = trainerService.findOne(ptMembershipSaveDto.getTrainerId());
+
+        PtMembership ptMembership = PtMembership.builder()
+                .norMember(loginedMember)
+                .trainer(trainer)
+                .initCount(ptMembershipSaveDto.getInitCount())
+                .remainPtCount(ptMembershipSaveDto.getInitCount())
+                .price(ptMembershipSaveDto.getPrice())
+                .build();
+
+        Long ptMembershipId = ptMembershipService.save(ptMembership);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(ptMembershipId));
+    }
+
+    /**
+     * 회원이 진행 중인 PT 리스트
+     */
+    @GetMapping("/n/list")
+    public ResponseEntity<ApiResponse<List<PtMembershipViewDto>>> ptMembershipByMember(
+            @SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) NorMember loginedMember
+    ) {
+        List<PtMembershipViewDto> ptMembershipList = ptMembershipService.findByMember(loginedMember).stream()
+                .map(pms -> PtMembershipViewDto.builder()
+                        .memberName(loginedMember.getName())
+                        .trainerId(pms.getTrainer().getId())
+                        .trainerName(pms.getTrainer().getName())
+                        .initCount(pms.getInitCount())
+                        .remainCount(pms.getRemainPtCount())
+                        .build())
+                .toList();
+
+        return ResponseEntity.ok().body(ApiResponse.success(ptMembershipList));
+    }
 }
--- src/main/java/project/gymnawa/controller/view/PtMembershipController.java
+++ src/main/java/project/gymnawa/controller/view/PtMembershipController.java
@@ -4,13 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import project.gymnawa.domain.dto.ptmembership.PtMembershipSaveDto;
 import project.gymnawa.domain.dto.ptmembership.PtMembershipViewDto;
 import project.gymnawa.domain.entity.NorMember;
 import project.gymnawa.domain.entity.PtMembership;
+import project.gymnawa.domain.entity.Trainer;
 import project.gymnawa.service.PtMembershipService;
+import project.gymnawa.service.TrainerService;
 import project.gymnawa.web.SessionConst;
 
 import java.util.List;
@@ -22,6 +25,51 @@ import java.util.List;
 public class PtMembershipController {
 
     private final PtMembershipService ptMembershipService;
+    private final TrainerService trainerService;
+
+    /**
+     * PT 등록 폼
+     */
+    @GetMapping("/register")
+    public String ptRegisterForm(@SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) NorMember loginedMember,
+                                 @RequestParam("trainerId") Long trainerId,
+                                 Model model) {
+
+        PtMembershipSaveDto ptMembershipSaveDto = PtMembershipSaveDto.builder()
+                .trainerId(trainerId)
+                .build();
+
+        model.addAttribute("ptMembershipSaveDto", ptMembershipSaveDto);
+
+        return "/ptmembership/ptRegisterForm";
+    }
+
+    /**
+     * PT 등록
+     */
+    @PostMapping("/register")
+    public String ptRegister(@SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) NorMember loginedMember,
+                             @Validated PtMembershipSaveDto ptMembershipSaveDto, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors = " + bindingResult);
+            return "/ptmembership/ptRegisterForm";
+        }
+
+        Trainer trainer = trainerService.findOne(ptMembershipSaveDto.getTrainerId());
+
+        PtMembership ptMembership = PtMembership.builder()
+                .norMember(loginedMember)
+                .trainer(trainer)
+                .initCount(ptMembershipSaveDto.getInitCount())
+                .remainPtCount(ptMembershipSaveDto.getInitCount())
+                .price(ptMembershipSaveDto.getPrice())
+                .build();
+
+        ptMembershipService.save(ptMembership);
+
+        return "redirect:/ptmembership/n/list";
+    }
 
     /**
      * 회원이 진행 중인 PT 조회
