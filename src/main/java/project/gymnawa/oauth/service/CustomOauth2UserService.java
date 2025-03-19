35b0040da6871d48bb876650ca9e31fa2dd4e045
chocoding <galmeagi2@naver.com>
2025-03-20 00:01:27 +0900

    oauth 로그인 후처리 service 생성

--- src/main/java/project/gymnawa/oauth/service/CustomOauth2UserService.java
+++ src/main/java/project/gymnawa/oauth/service/CustomOauth2UserService.java
@@ -1,2 +1,31 @@
-package project.gymnawa.oauth.service;public class CustomOauth2UserService {
+package project.gymnawa.oauth.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CustomOauth2UserService extends DefaultOAuth2UserService {
+
+    public CustomOauth2UserService() {
+        log.info("CustomOauth2UserService 빈 등록 완료");
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        System.out.println("loadUser 함수 진입");
+        log.info("loadUser 함수 진입");
+        log.info("getClientRegistration : " + userRequest.getClientRegistration());
+        log.info("getAccessToken : " + userRequest.getAccessToken());
+
+        return super.loadUser(userRequest);
+    }
 }
+
+
+
+
