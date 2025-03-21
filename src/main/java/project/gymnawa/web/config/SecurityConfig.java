011af6659c3bf605db1914e963a9af53e0283c20
chocoding <galmeagi2@naver.com>
2025-03-22 03:01:06 +0900

    CSRF 기능 off & pathVariable 정규 표현식 적용

--- src/main/java/project/gymnawa/web/config/SecurityConfig.java
+++ src/main/java/project/gymnawa/web/config/SecurityConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import project.gymnawa.oauth.service.CustomOauth2UserService;
 
@@ -23,9 +23,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-//                .csrf(CsrfConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/member/n/{id}/**", "/member/t/{id}/**").authenticated()
+                        .requestMatchers("/member/n/{id:\\d+}/**", "/member/t/{id:\\d+}/**").authenticated() // pathvariable은 정규 표현식 사용해야 함
                         .requestMatchers("/review/**").authenticated()
                         .requestMatchers("/gymtrainer/add", "gymtrainer/expire").authenticated()
                         .requestMatchers("/ptmembership/**").authenticated()
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .loginPage("/member/login")
                         .loginProcessingUrl("/member/login")
                         .defaultSuccessUrl("/")
+                        .usernameParameter("email")
                 )
                 .oauth2Login(oauth2 -> oauth2 // oauth2 로그인 설정
                         .loginPage("/member/login")
@@ -42,6 +43,11 @@ public class SecurityConfig {
                                 .userService(customOauth2UserService))
                 );
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
