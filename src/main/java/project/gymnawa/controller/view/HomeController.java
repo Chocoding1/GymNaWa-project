305e6e9e7ce72182f358b0bd4fe16141597352da
chocoding <galmeagi2@naver.com>
2025-03-22 02:58:19 +0900

    Spring Security 기반으로 코드 수정

--- src/main/java/project/gymnawa/controller/view/HomeController.java
+++ src/main/java/project/gymnawa/controller/view/HomeController.java
@@ -1,35 +1,50 @@
 package project.gymnawa.controller.view;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import project.gymnawa.domain.entity.Member;
 import project.gymnawa.domain.entity.NorMember;
+import project.gymnawa.oauth.domain.CustomUserDetails;
+import project.gymnawa.repository.MemberRepository;
+import project.gymnawa.service.MemberService;
 import project.gymnawa.web.SessionConst;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final MemberService memberService;
 
     @Value("${api.key}")
     private String apiKey;
 
     @GetMapping("/")
     public String home(
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginedMember,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
             Model model) {
 
-        model.addAttribute("apiKey", apiKey);
-        model.addAttribute("member", loginedMember);
+        if (customUserDetails != null) {
+            Member loginedMember = customUserDetails.getMember();
 
-        if (loginedMember instanceof NorMember) {
-            model.addAttribute("isTrainer", false);
-        } else {
-            model.addAttribute("isTrainer", true);
+            model.addAttribute("member", loginedMember);
+
+            if (loginedMember instanceof NorMember) {
+                model.addAttribute("isTrainer", false);
+            } else {
+                model.addAttribute("isTrainer", true);
+            }
         }
+
+        model.addAttribute("apiKey", apiKey);
 
         return "kakaoHome";
     }
