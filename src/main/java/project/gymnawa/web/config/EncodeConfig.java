34f528da4eaf4c353da6b7ea943bec4fdb7c9123
chocoding <galmeagi2@naver.com>
2025-03-24 15:58:56 +0900

    순환 참조 문제로 인한 config 파일 생성

--- src/main/java/project/gymnawa/web/config/EncodeConfig.java
+++ src/main/java/project/gymnawa/web/config/EncodeConfig.java
@@ -1,2 +1,14 @@
-package project.gymnawa.web.config;public class EncodeConfig {
+package project.gymnawa.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class EncodeConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
