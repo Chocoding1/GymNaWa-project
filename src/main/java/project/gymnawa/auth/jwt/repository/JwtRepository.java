e6bb09eada9c3c3466bd0abb26dc17b84bb82ebb
chocoding <galmeagi2@naver.com>
2025-04-17 08:47:13 +0900

    RT 저장할 Repository 객체

--- src/main/java/project/gymnawa/auth/jwt/repository/JwtRepository.java
+++ src/main/java/project/gymnawa/auth/jwt/repository/JwtRepository.java
@@ -1,2 +1,14 @@
-package project.gymnawa.auth.jwt.repository;public interface JwtRepository {
+package project.gymnawa.auth.jwt.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import project.gymnawa.auth.jwt.domain.RefreshToken;
+
+import java.util.Optional;
+
+@Repository
+public interface JwtRepository extends CrudRepository<RefreshToken, Long> {
+
+    @Override
+    Optional<RefreshToken> findById(Long userId);
 }
