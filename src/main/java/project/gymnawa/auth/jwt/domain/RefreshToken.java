320f8c1a91312aef5faf83a30b23b09afbadcd75
chocoding <galmeagi2@naver.com>
2025-04-17 08:46:23 +0900

    Redis에 저장할 RT 객체

--- src/main/java/project/gymnawa/auth/jwt/domain/RefreshToken.java
+++ src/main/java/project/gymnawa/auth/jwt/domain/RefreshToken.java
@@ -1,4 +1,19 @@
 package project.gymnawa.auth.jwt.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash(value = "refreshToken", timeToLive = 1209600) // timeToLive는 초단위
+@AllArgsConstructor
+@Builder
+@Getter
 public class RefreshToken {
+
+    @Id
+    private Long userId;
+    private String refreshToken;
+
 }
