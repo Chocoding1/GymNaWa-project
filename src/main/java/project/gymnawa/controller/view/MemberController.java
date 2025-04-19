d3c8ba5a8a89efe8482ea146390a6bba9d0c1b3f
chocoding <galmeagi2@naver.com>
2025-04-19 09:22:52 +0900

    추가 정보 입력 api 생성

--- src/main/java/project/gymnawa/controller/view/MemberController.java
+++ src/main/java/project/gymnawa/controller/view/MemberController.java
@@ -6,13 +6,23 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import project.gymnawa.auth.jwt.domain.JwtInfoDto;
 import project.gymnawa.auth.jwt.util.JwtUtil;
+import project.gymnawa.auth.oauth.domain.CustomOAuth2UserDetails;
+import project.gymnawa.domain.dto.member.MemberOauthInfoDto;
+import project.gymnawa.domain.dto.normember.MemberSaveDto;
+import project.gymnawa.domain.dto.trainer.TrainerSaveDto;
 import project.gymnawa.domain.entity.Member;
 import project.gymnawa.domain.dto.member.MemberLoginDto;
+import project.gymnawa.domain.entity.NorMember;
+import project.gymnawa.service.NorMemberService;
+import project.gymnawa.service.TrainerService;
 import project.gymnawa.web.SessionConst;
 import project.gymnawa.service.MemberService;
 
@@ -23,6 +33,8 @@ import project.gymnawa.service.MemberService;
 public class MemberController {
 
     private final MemberService memberService;
+    private final NorMemberService norMemberService;
+    private final TrainerService trainerService;
     private final JwtUtil jwtUtil;
 
     @GetMapping("/add/select")
@@ -77,6 +89,102 @@ public class MemberController {
 
         // redis의 refresh token 삭제
         jwtUtil.removeRefreshToken(id);
+
+        return "redirect:/";
+    }
+
+    /**
+     * 추가 정보 입력
+     */
+    @GetMapping("/add-info")
+    public String addInfoForm(@AuthenticationPrincipal CustomOAuth2UserDetails customOAuth2UserDetails,
+                          Model model,
+                          @ModelAttribute MemberOauthInfoDto memberOauthInfoDto) {
+
+        Long userId = customOAuth2UserDetails.getMember().getId();
+        Member loginedMember = memberService.findOne(userId);
+
+        model.addAttribute("name", loginedMember.getName());
+
+        return "/member/addInfoForm";
+    }
+
+    @PostMapping("/add-info")
+    public String addInfo(@AuthenticationPrincipal CustomOAuth2UserDetails customOAuth2UserDetails,
+                          @Validated MemberOauthInfoDto memberOauthInfoDto, BindingResult bindingResult,
+                          HttpServletRequest request, HttpServletResponse response) {
+
+        Long userId = customOAuth2UserDetails.getMember().getId();
+        Member loginedMember = memberService.findOne(userId);
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors = " + bindingResult);
+            return "/member/addInfoForm";
+        }
+
+        // 브라우저에서 쿠키 삭제
+        removeCookie(response, "access_token");
+        removeCookie(response, "refresh_token");
+
+        // redis에서 refresh token 삭제
+        String refreshToken = jwtUtil.resolveTokenFromCookie(request, "refresh_token");
+
+        Long id = jwtUtil.getId(refreshToken);
+
+        // redis의 refresh token 삭제
+        jwtUtil.removeRefreshToken(id);
+
+        Long newJoinId;
+        if (memberOauthInfoDto.getIsTrainer()) {
+            TrainerSaveDto trainerSaveDto = TrainerSaveDto.builder()
+                    .name(loginedMember.getName())
+                    .email(loginedMember.getEmail())
+                    .loginType(loginedMember.getLoginType())
+                    .provider(loginedMember.getProvider())
+                    .providerId(loginedMember.getProviderId())
+                    .gender(memberOauthInfoDto.getGender())
+                    .zoneCode(memberOauthInfoDto.getZoneCode())
+                    .address(memberOauthInfoDto.getAddress())
+                    .detailAddress(memberOauthInfoDto.getDetailAddress())
+                    .buildingName(memberOauthInfoDto.getBuildingName())
+                    .build();
+
+            memberService.deleteOne(userId);
+            newJoinId = trainerService.join(trainerSaveDto);
+        } else {
+            MemberSaveDto memberSaveDto = MemberSaveDto.builder()
+                    .name(loginedMember.getName())
+                    .email(loginedMember.getEmail())
+                    .loginType(loginedMember.getLoginType())
+                    .provider(loginedMember.getProvider())
+                    .providerId(loginedMember.getProviderId())
+                    .gender(memberOauthInfoDto.getGender())
+                    .zoneCode(memberOauthInfoDto.getZoneCode())
+                    .address(memberOauthInfoDto.getAddress())
+                    .detailAddress(memberOauthInfoDto.getDetailAddress())
+                    .buildingName(memberOauthInfoDto.getBuildingName())
+                    .build();
+
+            memberService.deleteOne(userId);
+            newJoinId = norMemberService.join(memberSaveDto);
+        }
+
+        JwtInfoDto jwtInfoDto = jwtUtil.createJwt(newJoinId);
+
+        Cookie accessCookie = new Cookie("access_token", jwtInfoDto.getAccessToken());
+        accessCookie.setHttpOnly(true);
+        accessCookie.setSecure(false);
+        accessCookie.setPath("/");
+        accessCookie.setMaxAge(60); // 1분
+
+        Cookie refreshCookie = new Cookie("refresh_token", jwtInfoDto.getRefreshToken());
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setSecure(false);
+        refreshCookie.setPath("/");
+        refreshCookie.setMaxAge(60 * 60 * 24 * 14); // 2주
+
+        response.addCookie(accessCookie);
+        response.addCookie(refreshCookie);
 
         return "redirect:/";
     }
