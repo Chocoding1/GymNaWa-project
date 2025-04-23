3a10978e3434c52d25b235ac3cfa6b3ca579957b
chocoding <galmeagi2@naver.com>
2025-04-23 16:51:29 +0900

    쿠키 생성 클래스 생성

--- src/main/java/project/gymnawa/auth/cookie/CookieUtil.java
+++ src/main/java/project/gymnawa/auth/cookie/CookieUtil.java
@@ -1,2 +1,28 @@
-package project.gymnawa.auth.cookie;public class CookieUtil {
+package project.gymnawa.auth.cookie;
+
+import jakarta.servlet.http.Cookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    public Cookie createAT(String accessToken) {
+        Cookie accessCookie = new Cookie("access_token", accessToken);
+        accessCookie.setHttpOnly(true);
+        accessCookie.setSecure(false);
+        accessCookie.setPath("/");
+        accessCookie.setMaxAge(60); // 1분
+
+        return accessCookie;
+    }
+
+    public Cookie createRT(String refreshToken) {
+        Cookie refreshCookie = new Cookie("refresh_token", refreshToken);
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setSecure(false);
+        refreshCookie.setPath("/");
+        refreshCookie.setMaxAge(60 * 60 * 24); // 24시간
+
+        return refreshCookie;
+    }
 }
