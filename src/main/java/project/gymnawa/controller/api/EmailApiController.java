0d13338174988b63cfa5280b66b7b0a4a01dcaa9
chocoding <galmeagi2@naver.com>
2025-05-09 22:44:51 +0900

    rest api 형태로 변경

--- src/main/java/project/gymnawa/controller/api/EmailApiController.java
+++ src/main/java/project/gymnawa/controller/api/EmailApiController.java
@@ -1,12 +1,14 @@
-package project.gymnawa.controller.view;
+package project.gymnawa.controller.api;
 
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.gymnawa.domain.api.ApiResponse;
 import project.gymnawa.domain.dto.email.EmailDto;
 import project.gymnawa.service.EmailService;
 
@@ -16,24 +18,28 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/email")
-public class EmailController {
+@RequestMapping("/api/email")
+public class EmailApiController {
 
     private final EmailService emailService;
 
     @PostMapping("/send-code")
-    public Map<String, Object> sendCode(@RequestBody EmailDto emailDto) throws MessagingException {
+    public ResponseEntity<ApiResponse<?>> sendCode(@RequestBody EmailDto emailDto) throws MessagingException {
         HashMap<String, Object> response = new HashMap<>();
 
         emailService.sendMail(emailDto.getEmail());
         response.put("success", true);
 
-        return response;
+        return ResponseEntity.ok().body(ApiResponse.success(response));
     }
 
     @PostMapping("verify-code")
-    public Map<String, Object> verifyCode(@RequestBody EmailDto emailDto) {
+    public ResponseEntity<ApiResponse<?>> verifyCode(@RequestBody EmailDto emailDto) {
         HashMap<String, Object> response = new HashMap<>();
+
+        log.info("verify-code 진입");
+        log.info("email : " + emailDto.getEmail());
+        log.info("code : " + emailDto.getCode());
 
         if (emailService.verifyCode(emailDto.getEmail(), emailDto.getCode())) {
             response.put("success", true);
@@ -41,7 +47,8 @@ public class EmailController {
             response.put("success", false);
             response.put("message", "인증코드가 올바르지 않습니다.");
         }
+        log.info("response 초기화 완료");
 
-        return response;
+        return ResponseEntity.ok().body(ApiResponse.success(response));
     }
 }
