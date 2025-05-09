572bc38c8dedc7764338ab3fbf561705f6b4e5ff
chocoding <galmeagi2@naver.com>
2025-05-09 23:00:11 +0900

    홈 화면용 DTO

--- src/main/java/project/gymnawa/domain/dto/member/MemberHomeInfoDto.java
+++ src/main/java/project/gymnawa/domain/dto/member/MemberHomeInfoDto.java
@@ -1,2 +1,15 @@
-package project.gymnawa.domain.dto.member;public class MemberHomeInfoDto {
+package project.gymnawa.domain.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class MemberHomeInfoDto {
+
+    private Long id;
+    private String name;
+    private boolean isTrainer;
 }
