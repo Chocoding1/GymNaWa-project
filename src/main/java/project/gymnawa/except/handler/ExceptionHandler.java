9818790a3693de779198c927ae131f0fd8d8e97a
chocoding <galmeagi2@naver.com>
2025-05-21 15:12:08 +0900

    에러 처리 핸들러 생성

--- src/main/java/project/gymnawa/except/handler/ExceptionHandler.java
+++ src/main/java/project/gymnawa/except/handler/ExceptionHandler.java
@@ -1,2 +1,11 @@
-package project.gymnawa.except.handler;public class ExceptionHandler {
+package project.gymnawa.except.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionHandler {
+
+
 }
