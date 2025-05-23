74335c2a2000a0a3090660bbef0e6db7ecae792e
chocoding <galmeagi2@naver.com>
2025-05-23 18:05:40 +0900

    Custom 예외 클래스 생성

--- src/main/java/project/gymnawa/errors/exception/CustomException.java
+++ src/main/java/project/gymnawa/errors/exception/CustomException.java
@@ -1,2 +1,15 @@
-package project.gymnawa.errors.error;public class CustomException {
+package project.gymnawa.errors.exception;
+
+import lombok.Getter;
+import project.gymnawa.errors.dto.ErrorCode;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+    }
 }
