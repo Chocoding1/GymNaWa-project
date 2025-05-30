c1062ff602795ce1570fda04c4fb6eb31506e16e
chocoding <galmeagi2@naver.com>
2025-05-30 13:49:08 +0900

    사용 x 라이브러리 제거

--- src/test/java/project/gymnawa/controller/api/HomeApiControllerTest.java
+++ src/test/java/project/gymnawa/controller/api/HomeApiControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
