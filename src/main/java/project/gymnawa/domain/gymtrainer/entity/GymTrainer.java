a786919e393e5e735b42a32a6e73ae0c136bac89
chocoding <galmeagi2@naver.com>
2025-06-26 21:03:29 +0900

    docs: 주석 추가

--- src/main/java/project/gymnawa/domain/gymtrainer/entity/GymTrainer.java
+++ src/main/java/project/gymnawa/domain/gymtrainer/entity/GymTrainer.java
@@ -23,6 +23,13 @@ public class GymTrainer {
     @JoinColumn(name = "TRAINER_ID")
     private Trainer trainer;
 
+    /**
+     * 현재 카카오 api에서 제공하는 체육관 정보를 그대로 이용하기 때문에 따로 객체 매핑을 하지 않음
+     * 그냥 카카오 api에서 제공하는 고유 번호를 저장하기 위해
+     * 객체 매핑을 위해서는 통일을 하는게 좋아보임
+     * 통일을 하기 위해서는 트레이너가 헬스장 등록 시, DB에 존재하는 헬스장이면 그대로 매핑,
+     * DB에 존재하지 않는 헬스장이면 id값만 설정하여 gym 객체 생성 후 매핑하는 방식으로 해야할 거 같다.
+     */
     private String gymId;
 
     private LocalDate hireDate;
