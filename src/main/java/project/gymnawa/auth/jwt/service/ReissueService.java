44bfbc81de24345b9ac83743cdd54094d0864a2c
chocoding <galmeagi2@naver.com>
2025-07-04 18:23:36 +0900

    docs: 주석 추가

--- src/main/java/project/gymnawa/auth/jwt/service/ReissueService.java
+++ src/main/java/project/gymnawa/auth/jwt/service/ReissueService.java
@@ -6,7 +6,10 @@ import org.springframework.http.ResponseEntity;
 
 public interface ReissueService {
 
+    // 일반 로그인용 메서드
+    // 반환 타입 ResponseENtity<ApiResponse<>>로 변경
     ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response);
 
+    // 소셜 로그인용 메서드
     ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response, Long userId);
 }
