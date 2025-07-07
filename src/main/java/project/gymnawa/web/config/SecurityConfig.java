54515a2c052eadce81cc187b91c58aea54d8e5d9
chocoding <galmeagi2@naver.com>
2025-07-07 22:30:25 +0900

    refactor: API 인가 재설정

--- src/main/java/project/gymnawa/web/config/SecurityConfig.java
+++ src/main/java/project/gymnawa/web/config/SecurityConfig.java
@@ -30,7 +30,7 @@ import java.util.List;
 
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // security 관련 설정이라는 것을 알려주는 어노테이션
 @RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig {
@@ -40,6 +40,7 @@ public class SecurityConfig {
     private final JwtUtil jwtUtil;
     private final CookieUtil cookieUtil;
     private final JwtRepository jwtRepository;
+    private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -47,8 +48,15 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        String[] permitUrls = {
+                "/login", "/logout", "/api/normembers", "/api/trainers",
+                "/api/reviews/{id:\\d+}", // pathvariable은 정규 표현식 사용해야 함
+                "/api/gyms"
+        };
+
         http
+                // mvc cors 설정도 해야 함
                 .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
 
                     @Override
@@ -69,22 +77,11 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // stateless 방식에서는 단순 소셜 로그인 진행해도 Authentication 객체 유지 X
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/member/n/{id:\\d+}/**", "/member/t/{id:\\d+}/**").authenticated() // pathvariable은 정규 표현식 사용해야 함
-                        .requestMatchers("/review/**").authenticated()
-                        .requestMatchers("/gymtrainer/add", "gymtrainer/expire").authenticated()
-                        .requestMatchers("/ptmembership/**").authenticated()
-                        .anyRequest().permitAll()
+                        .requestMatchers(permitUrls).permitAll()
+                        .anyRequest().authenticated()
                 )
                 .httpBasic(HttpBasicConfigurer::disable)
                 .formLogin(FormLoginConfigurer::disable) // jwt는 세션을 사용하지 않기 때문에 폼 로그인 기능 off
-/*
-                .formLogin(formLogin -> formLogin // 폼 로그인 설정
-                        .loginPage("/member/login")
-                        .loginProcessingUrl("/member/login")
-                        .defaultSuccessUrl("/")
-                        .usernameParameter("email")
-                )
-*/
                 .logout(LogoutConfigurer::disable) // 기존 로그아웃 필터 off
                 .oauth2Login(oauth2 -> oauth2 // oauth2 로그인 설정
                         .defaultSuccessUrl("/") // 이거 설정해줘야 홈 url에 Authentication 객체 전달됨
@@ -93,7 +90,7 @@ public class SecurityConfig {
                         .successHandler(customSuccessHandler)
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, cookieUtil), LoginFilter.class)
-                .addFilterAt(new LoginFilter(authenticationManager, jwtUtil, cookieUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, cookieUtil), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new CustomLogoutFilter(jwtUtil, jwtRepository), LogoutFilter.class);
 
         return http.build();
