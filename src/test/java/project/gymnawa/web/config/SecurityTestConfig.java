35e505495d4165bfa4950aedad74e9c596424d76
chocoding <galmeagi2@naver.com>
2025-07-07 22:44:26 +0900

    test: 테스트용 SecurityConfig 작성

--- src/test/java/project/gymnawa/web/config/SecurityTestConfig.java
+++ src/test/java/project/gymnawa/web/config/SecurityTestConfig.java
@@ -1,2 +1,26 @@
-package project.gymnawa.web.config;public class SecurityTestConfig {
+package project.gymnawa.web.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+public class SecurityTestConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(CsrfConfigurer::disable)
+                .formLogin(FormLoginConfigurer::disable)
+                .logout(LogoutConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
 }
