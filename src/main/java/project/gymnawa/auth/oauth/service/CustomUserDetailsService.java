58482ba6eb307fecf633331e158faeb037a2267c
chocoding <galmeagi2@naver.com>
2025-07-16 10:19:01 +0900

    Refactor: 필터용 커스텀 에러 변경

--- src/main/java/project/gymnawa/auth/oauth/service/CustomUserDetailsService.java
+++ src/main/java/project/gymnawa/auth/oauth/service/CustomUserDetailsService.java
@@ -6,9 +6,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import project.gymnawa.auth.jwt.error.CustomAuthException;
 import project.gymnawa.auth.oauth.domain.CustomOAuth2UserDetails;
 import project.gymnawa.domain.member.entity.Member;
-import project.gymnawa.domain.common.error.exception.CustomException;
 import project.gymnawa.domain.member.repository.MemberRepository;
 
 import static project.gymnawa.domain.common.error.dto.ErrorCode.*;
@@ -26,7 +26,9 @@ public class CustomUserDetailsService implements UserDetailsService {
         log.info("CustomUserDetailsService -> loadUserByUsername");
 
         Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+                .orElseThrow(() -> new CustomAuthException(LOGIN_FAILED));
+        // 로그인은 서블릿까지 가지 않고 필터 단에서 끝나기 때문에 오류 처리도 필터단에서 해야 한다.
+        // 따라서 필터단에서 잡을 수 있는 커스텀 에러를 발생시켰다.
 
         return new CustomOAuth2UserDetails(member);
     }
