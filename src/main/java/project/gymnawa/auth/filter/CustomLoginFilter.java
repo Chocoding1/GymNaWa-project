bfa265176ea3efc8e6a4ebbc6c19894d48ed0bc3
chocoding <galmeagi2@naver.com>
2025-07-16 10:02:10 +0900

    refactor: 필터용 커스텀 에러 변경 & JWT 발급 메서드 변경 & 로그인 실패 메서드 응답 양식 변경

--- src/main/java/project/gymnawa/auth/filter/CustomLoginFilter.java
+++ src/main/java/project/gymnawa/auth/filter/CustomLoginFilter.java
@@ -8,18 +8,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import project.gymnawa.auth.cookie.util.CookieUtil;
 import project.gymnawa.auth.jwt.dto.JwtInfoDto;
+import project.gymnawa.auth.jwt.error.CustomAuthException;
 import project.gymnawa.auth.jwt.util.JwtUtil;
 import project.gymnawa.auth.oauth.domain.CustomOAuth2UserDetails;
+import project.gymnawa.domain.common.error.dto.ErrorResponse;
 import project.gymnawa.domain.member.dto.MemberLoginDto;
 
 import java.io.IOException;
+
+import static project.gymnawa.domain.common.error.dto.ErrorCode.*;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -27,7 +30,7 @@ import java.io.IOException;
  * 해당 필터가 상속한 UsernamePasswordAuthenticationFilter는 POST /login 요청에만 작동하는 필터
  * 로그인 인증 성공 시(authorization code 발급 시), jWT 발급하는 필터
  */
-public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+public class CustomLoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
@@ -36,19 +39,14 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
-        log.info("로그인 시도 : LoginFilter 진입");
-        log.info("attemptAuthentication 진입");
-        log.info("Content-Type: " + request.getContentType());
+        log.info("CustomLoginFilter");
 
         ObjectMapper om = new ObjectMapper();
         MemberLoginDto memberLoginDto;
         try {
             memberLoginDto = om.readValue(request.getInputStream(), MemberLoginDto.class); // objectMapper 변환 시, 기본 생성자 필수로 있어야 됨
-            log.info("email : " + memberLoginDto.getEmail());
-            log.info("password : " + memberLoginDto.getPassword());
         } catch (IOException e) {
-            log.info("catch 진입");
-            throw new AuthenticationServiceException("LoginFilter(attemptAuthentication) error : 로그인 정보가 정확하지 않습니다.");
+            throw new CustomAuthException(LOGIN_FAILED);
         }
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(memberLoginDto.getEmail(), memberLoginDto.getPassword());
@@ -58,27 +56,28 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        log.info("CustomLoginFilter -> successfulAuthentication");
 
-        log.info("로그인 성공! successfulAuthentication 진입");
-        log.info("jwt 발급 시작");
         CustomOAuth2UserDetails oAuth2UserDetails = (CustomOAuth2UserDetails) authResult.getPrincipal();
-        System.out.println("oAuth2UserDetails = " + oAuth2UserDetails);
-        System.out.println("id : " + oAuth2UserDetails.getId());
-        JwtInfoDto jwtInfoDto = jwtUtil.createJwt(oAuth2UserDetails.getId());
-        log.info("refresh token 생성 : " + jwtInfoDto.getRefreshToken());
+        String refreshToken = jwtUtil.createRefreshToken(oAuth2UserDetails.getId());
 
 //        Cookie accessCookie = cookieUtil.createAT(jwtInfoDto.getAccessToken());
 //        ResponseCookie refreshCookie = cookieUtil.createRT(jwtInfoDto.getRefreshToken());
 
 //        response.addCookie(accessCookie);
 //        response.setHeader("Set-Cookie", refreshCookie.toString());
-        response.setHeader("Authorization-Refresh", jwtInfoDto.getRefreshToken());
+        response.setHeader("Authorization-Refresh", refreshToken);
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        log.info("로그인 실패..");
-        log.info("reason : " + failed.getMessage());
-        response.sendError(401, "존재하지 않는 회원입니다.");
+        log.info("CustomLoginFilter -> unsuccessfulAuthentication");
+
+        ObjectMapper om = new ObjectMapper();
+        ErrorResponse errorResponse = ErrorResponse.of("LOGIN_FAILED", "이메일 또는 비밀번호가 일치하지 않습니다.");
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(om.writeValueAsString(errorResponse));
     }
 }
