70bbd6a838ba8b200c4aa9dbe103fe8569e0ad75
chocoding <galmeagi2@naver.com>
2025-07-16 10:09:00 +0900

    Refactor: 커스텀 에러명 변경

--- src/main/java/project/gymnawa/auth/jwt/error/CustomAuthException.java
+++ src/main/java/project/gymnawa/auth/jwt/error/CustomAuthException.java
@@ -4,11 +4,11 @@ import lombok.Getter;
 import project.gymnawa.domain.common.error.dto.ErrorCode;
 
 @Getter
-public class CustomJwtException extends RuntimeException {
+public class CustomAuthException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
-    public CustomJwtException(ErrorCode errorCode) {
+    public CustomAuthException(ErrorCode errorCode) {
         super(errorCode.getErrorMessage());
         this.errorCode = errorCode;
     }
