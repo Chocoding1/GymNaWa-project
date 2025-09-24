a46c8259cae4d222ab5faf1137ba60d0fca8c8c8
chocoding <galmeagi2@naver.com>
2025-09-24 11:55:38 +0900

    Refactor: 헬스장 검색용 DTO 생성

--- src/main/java/project/gymnawa/domain/gym/dto/GymSearchRequestDto.java
+++ src/main/java/project/gymnawa/domain/gym/dto/GymSearchRequestDto.java
@@ -1,2 +1,13 @@
-package project.gymnawa.domain.gym.dto;public class GymSearchRequestDto {
+package project.gymnawa.domain.gym.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class GymSearchRequestDto {
+
+    private Double x;
+    private Double y;
+    private String keyword;
 }
