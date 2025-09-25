e5217cd9687a382c4f9a0cacad796b7ddf6e6598
chocoding <galmeagi2@naver.com>
2025-09-25 14:32:13 +0900

    Refactor: 메서드 파라미터, 반환 타입 변경

--- src/main/java/project/gymnawa/auth/jwt/service/ReissueService.java
+++ src/main/java/project/gymnawa/auth/jwt/service/ReissueService.java
@@ -3,13 +3,14 @@ package project.gymnawa.auth.jwt.service;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+import project.gymnawa.auth.jwt.dto.JwtInfoDto;
 
 public interface ReissueService {
 
     // 일반 로그인용 메서드
     // 반환 타입 ResponseENtity<ApiResponse<>>로 변경
-    ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response);
+    JwtInfoDto reissue(String refreshToken);
 
     // 소셜 로그인용 메서드
-    ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response, Long userId);
+    JwtInfoDto reissue(String refreshToken, Long userId);
 }
