7ffefb6a30b85294ad531a3e7c0a77ff03f5eb97
chocoding <galmeagi2@naver.com>
2025-09-25 14:42:22 +0900

    Refactor: Service단과 역할 분리

--- src/main/java/project/gymnawa/auth/jwt/controller/ReissueController.java
+++ src/main/java/project/gymnawa/auth/jwt/controller/ReissueController.java
@@ -4,10 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.gymnawa.auth.jwt.dto.JwtInfoDto;
 import project.gymnawa.auth.jwt.service.ReissueServiceImpl;
+import project.gymnawa.domain.common.api.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +20,14 @@ public class ReissueController {
     private final ReissueServiceImpl reissueServiceImpl;
 
     @PostMapping("/reissue")
-    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
-        ResponseEntity<?> reissue = reissueServiceImpl.reissue(request, response);
-        return reissue;
+    public ResponseEntity<ApiResponse<Void>> reissue(HttpServletRequest request, HttpServletResponse response) {
+
+        String refreshToken = request.getHeader("Authorization-Refresh");
+        JwtInfoDto jwtInfoDto = reissueServiceImpl.reissue(refreshToken);
+
+        response.setHeader("Authorization", "Bearer " + jwtInfoDto.getAccessToken());
+        response.setHeader("Authorization-Refresh", jwtInfoDto.getRefreshToken());
+
+        return ResponseEntity.ok(ApiResponse.of("토큰이 재발급되었습니다."));
     }
 }
