5002070e7a5dbcedb2dfdc1e72e68499c54ae348
chocoding <galmeagi2@naver.com>
2025-09-26 14:51:14 +0900

    Test: ReviewService 단위 테스트 수정 완료

--- src/test/java/project/gymnawa/domain/review/service/ReviewServiceTest.java
+++ src/test/java/project/gymnawa/domain/review/service/ReviewServiceTest.java
@@ -7,9 +7,11 @@ import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
+import project.gymnawa.domain.normember.service.NorMemberService;
+import project.gymnawa.domain.review.dto.ReviewEditDto;
 import project.gymnawa.domain.review.dto.ReviewSaveDto;
 import project.gymnawa.domain.normember.entity.NorMember;
+import project.gymnawa.domain.review.dto.ReviewViewDto;
 import project.gymnawa.domain.review.entity.Review;
 import project.gymnawa.domain.trainer.entity.Trainer;
 import project.gymnawa.domain.common.error.dto.ErrorCode;
@@ -20,9 +22,9 @@ import project.gymnawa.domain.trainer.service.TrainerService;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static project.gymnawa.domain.common.error.dto.ErrorCode.*;
 
 @ExtendWith(MockitoExtension.class)
 class ReviewServiceTest {
@@ -33,116 +35,161 @@ class ReviewServiceTest {
     @Mock
     ReviewRepository reviewRepository;
     @Mock
+    NorMemberService norMemberService;
+    @Mock
     TrainerService trainerService;
 
     @Test
     @DisplayName("리뷰 저장")
-    void save() {
+    void save_success() {
         //given
-        NorMember norMember = createNorMember("galmeagi2@naver.com", "1234", "조성진");
+        long userId = 1L;
+        NorMember norMember = NorMember.builder().id(userId).build();
+        Trainer trainer = Trainer.builder().id(2L).build();
+        Review review = Review.builder().build();
 
         ReviewSaveDto reviewSaveDto = ReviewSaveDto.builder()
                 .content("content")
-                .trainerId(1L)
+                .trainerId(2L)
                 .build();
 
-        when(reviewRepository.save(any(Review.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        when(norMemberService.findOne(anyLong())).thenReturn(norMember);
+        when(trainerService.findOne(anyLong())).thenReturn(trainer);
+        when(reviewRepository.save(any(Review.class))).thenReturn(review);
 
         //when
-        reviewService.save(reviewSaveDto, norMember);
+        reviewService.save(reviewSaveDto, userId);
 
         //then
+        verify(norMemberService, times(1)).findOne(anyLong());
         verify(trainerService, times(1)).findOne(anyLong());
         verify(reviewRepository, times(1)).save(any(Review.class));
     }
 
     @Test
-    @DisplayName("회원 별 리뷰 조회")
-    void findByMember() {
+    @DisplayName("리뷰 저장 실패 - 회원 조회 중 오류 발생")
+    void save_fail_norMemberServiceThrowsException() {
         //given
-        NorMember norMember = createNorMember("galmeagi2@naver.com", "1234", "조성진");
+        long userId = 1L;
+
+        ReviewSaveDto reviewSaveDto = ReviewSaveDto.builder()
+                .content("content")
+                .trainerId(2L)
+                .build();
+
+        when(norMemberService.findOne(userId)).thenThrow(new CustomException(MEMBER_NOT_FOUND));
 
         //when
-        List<Review> result = reviewService.findByMember(norMember);
+        CustomException customException = assertThrows(CustomException.class, () -> reviewService.save(reviewSaveDto, userId));
 
         //then
-        verify(reviewRepository, times(1)).findByNorMember(norMember);
+        ErrorCode errorCode = customException.getErrorCode();
+        assertEquals(MEMBER_NOT_FOUND, errorCode);
+
+        verify(norMemberService, times(1)).findOne(anyLong());
+        verify(trainerService, never()).findOne(anyLong());
+        verify(reviewRepository, never()).save(any(Review.class));
     }
 
     @Test
-    @DisplayName("트레이너 별 리뷰 조회")
-    void findByTrainer() {
+    @DisplayName("리뷰 저장 실패 - 트레이너 조회 중 오류 발생")
+    void save_fail_trainerServiceThrowsException() {
         //given
-        Trainer trainer = createTrainer("galmeagi2@naver.com", "456", "조성모");
+        long userId = 1L;
+        NorMember norMember = NorMember.builder().id(userId).build();
+
+        ReviewSaveDto reviewSaveDto = ReviewSaveDto.builder()
+                .content("content")
+                .trainerId(2L)
+                .build();
+
+        when(norMemberService.findOne(userId)).thenReturn(norMember);
+        when(trainerService.findOne(2L)).thenThrow(new CustomException(MEMBER_NOT_FOUND));
 
         //when
-        reviewService.findByTrainer(trainer);
+        CustomException customException = assertThrows(CustomException.class, () -> reviewService.save(reviewSaveDto, userId));
 
         //then
-        verify(reviewRepository, times(1)).findByTrainer(trainer);
+        ErrorCode errorCode = customException.getErrorCode();
+        assertEquals(MEMBER_NOT_FOUND, errorCode);
+
+        verify(norMemberService, times(1)).findOne(anyLong());
+        verify(trainerService, times(1)).findOne(anyLong());
+        verify(reviewRepository, never()).save(any(Review.class));
     }
 
     @Test
     @DisplayName("리뷰 수정 성공")
     void updateReviewSuccess() {
         //given
-        NorMember norMember = createNorMember("galmeagi2@naver.com", "1234", "조성진");
+        Long userId = 100L;
+        NorMember norMember = NorMember.builder().id(userId).build();
         Long reviewId = 1L;
         Review review = Review.builder()
+                .id(reviewId)
                 .content("oldContent")
                 .norMember(norMember)
+                .trainer(Trainer.builder().build())
                 .build();
-        String newContent = "newContent";
+        ReviewEditDto reviewEditDto = ReviewEditDto.builder().content("newContent").build();
 
-        when(reviewRepository.findByIdAndNorMember(anyLong(), any(NorMember.class))).thenReturn(Optional.of(review));
+        when(norMemberService.findOne(userId)).thenReturn(norMember);
+        when(reviewRepository.findByIdAndNorMember(reviewId, norMember)).thenReturn(Optional.of(review));
 
         //when
-        reviewService.updateReview(reviewId, norMember, newContent);
+        ReviewViewDto result = reviewService.updateReview(reviewId, userId, reviewEditDto);
 
         //then
-        assertThat(review.getContent()).isEqualTo(newContent);
+        assertNotNull(result);
+        assertEquals("newContent", result.getContent());
 
-        verify(reviewRepository, times(1)).findByIdAndNorMember(anyLong(), any(NorMember.class));
+        verify(norMemberService, times(1)).findOne(userId);
+        verify(reviewRepository, times(1)).findByIdAndNorMember(reviewId, norMember);
     }
 
     @Test
     @DisplayName("리뷰 수정 실패 - 존재하지 않는 리뷰")
     void updateReviewFail() {
         //given
-        NorMember norMember = createNorMember("galmeagi2@naver.com", "1234", "조성진");
-        String newContent = "newContent";
+        Long userId = 100L;
+        NorMember norMember = NorMember.builder().id(userId).build();
+        ReviewEditDto reviewEditDto = ReviewEditDto.builder().content("newContent").build();
         Long reviewId = 1L;
-        when(reviewRepository.findByIdAndNorMember(anyLong(), any(NorMember.class))).thenReturn(Optional.empty());
+
+        when(norMemberService.findOne(userId)).thenReturn(norMember);
+        when(reviewRepository.findByIdAndNorMember(reviewId, norMember)).thenReturn(Optional.empty());
 
         //when & then
         CustomException customException = assertThrows(CustomException.class,
-                () -> reviewService.updateReview(reviewId, norMember, newContent));
+                () -> reviewService.updateReview(reviewId, userId, reviewEditDto));
         ErrorCode errorCode = customException.getErrorCode();
 
-        verify(reviewRepository, times(1)).findByIdAndNorMember(anyLong(), any(NorMember.class));
+        assertEquals(REVIEW_NOT_FOUND, errorCode);
 
-        assertThat(errorCode.getCode()).isEqualTo("REVIEW_NOT_FOUND");
-        assertThat(errorCode.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
-        assertThat(errorCode.getErrorMessage()).isEqualTo("존재하지 않는 리뷰입니다.");
+        verify(norMemberService, times(1)).findOne(userId);
+        verify(reviewRepository, times(1)).findByIdAndNorMember(reviewId, norMember);
     }
 
     @Test
     @DisplayName("리뷰 삭제 성공")
     void ReviewDeleteSuccess() {
         //given
-        NorMember norMember = createNorMember("galmeagi2@naver.com", "1234", "조성진");
+        Long userId = 100L;
+        NorMember norMember = NorMember.builder().id(userId).build();
         Long reviewId = 1L;
         Review review = Review.builder()
                 .content("oldContent")
                 .norMember(norMember)
                 .build();
 
+        when(norMemberService.findOne(userId)).thenReturn(norMember);
         when(reviewRepository.findByIdAndNorMember(anyLong(), any(NorMember.class))).thenReturn(Optional.of(review));
 
         //when
-        reviewService.deleteReview(reviewId, norMember);
+        reviewService.deleteReview(reviewId, userId);
 
         //then
+        verify(norMemberService, times(1)).findOne(userId);
         verify(reviewRepository, times(1)).findByIdAndNorMember(anyLong(), any(NorMember.class));
         verify(reviewRepository, times(1)).delete(any(Review.class));
 
@@ -156,37 +203,54 @@ class ReviewServiceTest {
     void ReviewDeleteFail() {
         //given
         Long reviewId = 1L;
-        NorMember norMember = createNorMember("galmeagi2@naver.com", "1234", "조성진");
+        Long userId = 100L;
+        NorMember norMember = NorMember.builder().id(userId).build();
 
+        when(norMemberService.findOne(userId)).thenReturn(norMember);
         when(reviewRepository.findByIdAndNorMember(anyLong(), any(NorMember.class))).thenReturn(Optional.empty());
 
         //when & then
         CustomException customException = assertThrows(CustomException.class,
-                () -> reviewService.deleteReview(reviewId, norMember));
-        ErrorCode errorCode = customException.getErrorCode();
+                () -> reviewService.deleteReview(reviewId, userId));
 
         //then
+        ErrorCode errorCode = customException.getErrorCode();
+        assertEquals(REVIEW_NOT_FOUND, errorCode);
+
+        verify(norMemberService, times(1)).findOne(userId);
         verify(reviewRepository, times(1)).findByIdAndNorMember(anyLong(), any(NorMember.class));
         verify(reviewRepository, never()).delete(any(Review.class));
-
-        assertThat(errorCode.getCode()).isEqualTo("REVIEW_NOT_FOUND");
-        assertThat(errorCode.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
-        assertThat(errorCode.getErrorMessage()).isEqualTo("존재하지 않는 리뷰입니다.");
     }
 
-    private NorMember createNorMember(String email, String password, String name) {
-        return NorMember.builder()
-                .email(email)
-                .password(password)
-                .name(name)
+    @Test
+    @DisplayName("트레이너 별 리뷰 조회 성공")
+    void findByTrainer_success() {
+        //given
+        Long userId = 2L;
+        NorMember norMember = NorMember.builder().id(userId).build();
+        Long trainerId = 1L;
+        Trainer trainer = Trainer.builder().id(trainerId).build();
+        Review review1 = Review.builder()
+                .content("review1")
+                .norMember(norMember)
+                .trainer(trainer)
                 .build();
-    }
+        Review review2 = Review.builder()
+                .content("review2")
+                .norMember(norMember)
+                .trainer(trainer)
+                .build();
 
-    private Trainer createTrainer(String email, String password, String name) {
-        return Trainer.builder()
-                .email(email)
-                .password(password)
-                .name(name)
-                .build();
+        when(trainerService.findOne(trainerId)).thenReturn(trainer);
+        when(reviewRepository.findByTrainer(trainer)).thenReturn(List.of(review1, review2));
+
+        //when
+        List<ReviewViewDto> result = reviewService.findByTrainer(trainerId);
+
+        //then
+        assertEquals(2, result.size());
+
+        verify(trainerService, times(1)).findOne(trainerId);
+        verify(reviewRepository, times(1)).findByTrainer(trainer);
     }
 }
