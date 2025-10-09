464dd0932d4a8ec5cd4a562c8e0be8c3398d46b8
chocoding <galmeagi2@naver.com>
2025-10-09 15:22:56 +0900

    Docs: 주석 추가

--- src/main/java/project/gymnawa/config/JpaAuditingConfig.java
+++ src/main/java/project/gymnawa/config/JpaAuditingConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing // entity 생성, 변경 시 시간, 사용자 추적용
 public class JpaAuditingConfig {
 }
